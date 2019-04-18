88ce432703754d0089cb3c83bd79eb142f06ea00
SamyOptimize <48402367+SamyOptimize@users.noreply.github.com>
2019-04-18 17:11:32 +0300

    Update index.js

--- components/Screens/Signup/MainForm/index.js
+++ components/Screens/Signup/MainForm/index.js
@@ -23,8 +23,8 @@ class MainForm extends Component {
   state = { verified: false };
   render() {
     let title = "Phone Number";
-    let content = <BusinessInfo navigation={this.props.navigation} />;
-    //  <PhoneNo navigation={this.props.navigation} />;
+    let content =  <PhoneNo navigation={this.props.navigation} />;
+   
     if (this.props.verificationCode) {
       content = <Verification />;
       title = "Verification";
