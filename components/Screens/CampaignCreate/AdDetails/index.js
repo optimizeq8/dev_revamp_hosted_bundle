76cf47a15b95c15f8a4cc11b2e6276e145933050
SamyOptimize <samy@optimizekw.com>
2019-05-22 13:28:42 +0300

    Fixed areas secions in billing address, fixed errors in address post/put

--- components/Screens/CampaignCreate/AdDetails/index.js
+++ components/Screens/CampaignCreate/AdDetails/index.js
@@ -209,7 +209,7 @@ class AdDetails extends Component {
       campaignInfo: rep
     });
   };
-  onSelectedCountryChange = async (selectedItem, countryName) => {
+  onSelectedCountryChange = async (selectedItem, mounting, countryName) => {
     console.log(selectedItem);
 
     let replace = this.state.campaignInfo;
