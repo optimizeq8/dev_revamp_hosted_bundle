5afa0061f39cac26570d8d799232b7701f063704
SamyOptimize <samy@optimizekw.com>
2019-08-06 13:23:34 +0300

    version publish bump

--- components/Screens/Menu/index.js
+++ components/Screens/Menu/index.js
@@ -215,7 +215,7 @@ class Menu extends Component {
               <Text style={styles.text}>Logout</Text>
             </TouchableOpacity>
             <Text style={styles.version}>
-              Version:{Constants.manifest.version}3/
+              Version:{Constants.manifest.version}5/
               {Constants.manifest.ios.buildNumber}/
               {Constants.manifest.android.versionCode}
             </Text>
