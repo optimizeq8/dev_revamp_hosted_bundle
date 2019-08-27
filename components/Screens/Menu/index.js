91d6a879b8a88e10a5f399ee72afdd8ef072ed5d
sh-optimize <shorook@optimizekw.com>
2019-08-27 13:20:20 +0300

    update version to 12: added name feild to the invite code request, fixed the regx for websit.

--- components/Screens/Menu/index.js
+++ components/Screens/Menu/index.js
@@ -215,7 +215,7 @@ class Menu extends Component {
               <Text style={styles.text}>Logout</Text>
             </TouchableOpacity>
             <Text style={styles.version}>
-              Version:{Constants.manifest.version}11/
+              Version:{Constants.manifest.version}12/
               {Constants.manifest.ios.buildNumber}/
               {Constants.manifest.android.versionCode}
             </Text>
