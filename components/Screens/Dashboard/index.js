5b538c9c30fac8c2baa04c73ef00214308936778
SamyOptimize <samy@optimizekw.com>
2019-09-30 14:21:11 +0300

    fixed issue with emptycampaign screen showing while menu is is open

--- components/Screens/Dashboard/index.js
+++ components/Screens/Dashboard/index.js
@@ -29,6 +29,7 @@ import FilterIcon from "../../../assets/SVGs/Filter.svg";
 import IntercomIcon from "../../../assets/SVGs/IntercomIcon.svg";
 import BackdropIcon from "../../../assets/SVGs/BackDropIcon";
 import * as Icons from "../../../assets/SVGs/MenuIcons/index";
+import Background from "../../../assets/SVGs/Background";
 
 // Style
 import styles from "./styles";
@@ -286,6 +287,11 @@ class Dashboard extends Component {
           {/* {this.state.anim && ( */}
           <BackdropIcon style={styles.backDrop} height={hp("100%")} />
           {/* )} */}
+          <Background
+            style={[styles.background]}
+            width={wp(85)}
+            height={hp(61)}
+          />
           {!this.state.sidemenustate && (
             <View
               style={[
@@ -541,8 +547,14 @@ class Dashboard extends Component {
                   Segment.screen("Dashboard");
                 }
               }}
-              duration={50}
-              animation={!this.state.sidemenustate ? "fadeIn" : "fadeOut"}
+              duration={100}
+              animation={
+                (this.props.campaignList.length === 0 && this.state.anim) ||
+                (!this.state.sidemenustate &&
+                  this.props.campaignList.length !== 0)
+                  ? "fadeIn"
+                  : "fadeOut"
+              }
               style={styles.menuContainer}
             >
               <Menu
