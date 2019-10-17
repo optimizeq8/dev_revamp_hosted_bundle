312f5e5dcbe65bef534d21cfc22c25462cb3301a
SamyOptimize <samy@optimizekw.com>
2019-10-17 12:25:13 +0300

    dev version publish bump

--- components/Screens/Menu/index.js
+++ components/Screens/Menu/index.js
@@ -251,7 +251,7 @@ class Menu extends Component {
               </TouchableOpacity>
               <Text style={styles.version}>
                 {translate("Version:")}
-                {Constants.manifest.version}50/
+                {Constants.manifest.version}51/
                 {Constants.manifest.ios.buildNumber}/
                 {Constants.manifest.android.versionCode}
               </Text>
