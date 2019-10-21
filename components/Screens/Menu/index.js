a8eac2b678438921a11015483b862ebdbfa18ad9
SamyOptimize <samy@optimizekw.com>
2019-10-21 17:18:52 +0300

    dev version publish bump for expo35

--- components/Screens/Menu/index.js
+++ components/Screens/Menu/index.js
@@ -251,7 +251,7 @@ class Menu extends Component {
               </TouchableOpacity>
               <Text style={styles.version}>
                 {translate("Version:")}
-                {Constants.manifest.version}54/
+                {Constants.manifest.version}55/
                 {Constants.manifest.ios.buildNumber}/
                 {Constants.manifest.android.versionCode}
               </Text>
