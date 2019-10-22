10626645f9d8b68fe76ef7da8dd9dfd83e030f70
SamyOptimize <samy@optimizekw.com>
2019-10-22 19:46:01 +0300

    added ota update checker

--- components/Screens/AppUpdateChecker/index.js
+++ components/Screens/AppUpdateChecker/index.js
@@ -1,6 +1,6 @@
 //Components
 import React, { Component } from "react";
-import { Linking } from "expo";
+import { Linking, Updates } from "expo";
 import Constants from "expo-constants";
 import * as Animatable from "react-native-animatable";
 const imageLogo = require("../../../assets/images/logo01.png");
@@ -14,6 +14,7 @@ import {
 import Tutorial from "../Tutorial";
 import { globalColors } from "../../../GlobalStyles";
 import { Button } from "native-base";
+import { showMessage } from "react-native-flash-message";
 class AppUpdateChecker extends Component {
   static navigationOptions = {
     header: null
@@ -21,25 +22,55 @@ class AppUpdateChecker extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      updateDownloaded: false
+      updateDownloaded: false,
+      status: "",
+      status2: "",
+      statusLoading: false
     };
   }
   async componentDidMount() {
-    if (Constants.manifest.version === "0.1.61")
-      this.setState({ updateDownloaded: true });
+    if (Constants.manifest.version === "0.1.61") {
+    }
 
-    // try {
-    //   const update = await Updates.checkForUpdateAsync();
-    //   if (update.isAvailable) {
-    //     await Updates.fetchUpdateAsync();
-    //     this.setState({ updateDownloaded: true });
-    //     Updates.reloadFromCache();
-    //   }
-    // } catch (e) {
-    //   console.log("error", e);
-    // }
+    try {
+      this.setState({ status: "Checking for updates.", statusLoading: true });
+      const update = await Updates.checkForUpdateAsync();
+      if (update.isAvailable) {
+        this.setState({
+          status:
+            "There is an update available. Please wait while it downloads."
+        });
+        await Updates.fetchUpdateAsync({
+          eventListener: this.handleOTAListener
+        });
+        // ... notify user of update ...
+        Updates.reloadFromCache();
+      } else {
+        this.setState({ updateDownloaded: true });
+      }
+    } catch (e) {
+      alert(e.message || e.response || "Something went wrong", [
+        {
+          text: "Reload",
+          onPress: () => Updates.reload()
+        }
+      ]);
+
+      // handle or log error
+    }
   }
 
+  handleOTAListener = event => {
+    if (event.type === Updates.EventType.DOWNLOAD_STARTED) {
+      this.setState({
+        status2: "Downloading..."
+      });
+    } else if (event.type === Updates.EventType.DOWNLOAD_FINISHED) {
+      this.setState({
+        status2: "Download done..."
+      });
+    }
+  };
   render() {
     const { translate } = this.props.screenProps;
     if (this.state.updateDownloaded) {
@@ -74,18 +105,6 @@ class AppUpdateChecker extends Component {
             resizeMode="contain"
           />
 
-          {/* <Image
-            style={{
-              width: "100%",
-              height: "100%",
-              // top: 0,
-              // left: 0,
-              // bottom: 0,
-              // right: 0,
-              resizeMode: "cover"
-            }}
-            source={require("../../../assets/splash.png")}
-          /> */}
           <Animatable.View
             animation={"slideInUp"}
             style={{
@@ -110,6 +129,7 @@ class AppUpdateChecker extends Component {
             >
               {translate("Update Available")}
             </Text>
+
             <Text
               style={{
                 fontFamily: "montserrat-light",
@@ -118,9 +138,8 @@ class AppUpdateChecker extends Component {
                 fontSize: 16
               }}
             >
-              {translate(
-                "You appear to be using an outdated version of OptimizeApp, please update to the latest version to enjoy all our features!"
-              )}
+              status1: {this.state.status + "\n"}
+              status2: {this.state.status2}
             </Text>
           </Animatable.View>
           <Button
@@ -151,24 +170,26 @@ class AppUpdateChecker extends Component {
               {translate("Update Now")}
             </Text>
           </Button>
-          {/* <LottieView
-            ref={animation => {
-              this.animation = animation;
-            }}
-            style={{
-              zIndex: 10,
-              alignSelf: "center",
-              position: "absolute",
-              width: widthPercentageToDP(20),
-              height: widthPercentageToDP(20),
-              bottom: 20
-              // position: "absolute"
-            }}
-            resizeMode="contain"
-            source={require("../../../assets/animation/update_loader.json")}
-            loop
-            autoPlay
-          /> */}
+          {this.state.statusLoading && (
+            <LottieView
+              ref={animation => {
+                this.animation = animation;
+              }}
+              style={{
+                zIndex: 10,
+                alignSelf: "center",
+                position: "absolute",
+                width: widthPercentageToDP(20),
+                height: widthPercentageToDP(20),
+                bottom: 20
+                // position: "absolute"
+              }}
+              resizeMode="contain"
+              source={require("../../../assets/animation/update_loader.json")}
+              loop
+              autoPlay
+            />
+          )}
         </View>
       );
   }
