348a93c16fddc307a742f37063b3f9c32a025678
SamyOptimize <samy@optimizekw.com>
2019-10-22 15:06:56 +0300

    added action and reducer for checking update version

--- store/reducers/genericReducer.js
+++ store/reducers/genericReducer.js
@@ -1,9 +1,13 @@
 import * as actionTypes from "../actions/actionTypes";
 
-const initialState = {};
+const initialState = { actualVersion: "", loadingChecker: false };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case actionTypes.SET_ACTUAL_VERSION:
+      return { ...state, actualVersion: action.payload, loadingChecker: false };
+    case actionTypes.SET_UPDATE_LOADING:
+      return { ...state, loadingChecker: action.payload };
     default:
       return state;
   }
