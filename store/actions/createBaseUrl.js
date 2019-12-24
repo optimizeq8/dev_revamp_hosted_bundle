e87a20d6dc97186c880b8170b642645a8b4220d8
SamyOptimize <samy@optimizekw.com>
2019-12-24 11:20:48 +0300

    Add promise rejection in createbaseurl errorHandler

--- store/actions/createBaseUrl.js
+++ store/actions/createBaseUrl.js
@@ -19,6 +19,7 @@ export default () => {
   const errorHandler = error => {
     if (error) {
       console.log("errorHandler", error);
+      return Promise.reject(error);
     }
     return error;
   };
