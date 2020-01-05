ee722e0fb1b46ac7b8060e91d16283479596e9e2
sh-optimize <shorook@optimizekw.com>
2020-01-05 13:07:48 +0300

    bump to ver 87

--- components/Screens/Menu/index.js
+++ components/Screens/Menu/index.js
@@ -306,7 +306,7 @@ class Menu extends Component {
               </TouchableOpacity>
               <Text style={styles.version}>
                 {translate("Version:")}
-                {Constants.manifest.version}/86/
+                {Constants.manifest.version}/87/
                 {Constants.manifest.ios.buildNumber}/
                 {Constants.manifest.android.versionCode}
               </Text>
