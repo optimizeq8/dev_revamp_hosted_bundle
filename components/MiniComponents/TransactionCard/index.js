fcfd2dc0e361711889f657b3f5da1eaf7b6c428f
saadiyakazi <saadiya@optimizekw.com>
2020-01-20 17:16:25 +0300

    Transaction List Screen Design Changes as in XD

--- components/MiniComponents/TransactionCard/index.js
+++ components/MiniComponents/TransactionCard/index.js
@@ -25,62 +25,72 @@ class TransactionCard extends Component {
     const { translate } = this.props.screenProps;
     return (
       <View style={styles.cardStyle}>
-        <View style={styles.transactionButton}>
-          <View style={styles.textcontainer}>
-            <View style={styles.header}>
-              <Text
-                numberOfLines={2}
-                style={[
-                  styles.titleText,
-                  transaction.campaign_name !== "Wallet Topup" &&
-                  !isStringArabic(transaction.campaign_name)
-                    ? {
-                        fontFamily: "montserrat-bold-english"
-                      }
-                    : {}
-                ]}
-              >
-                {transaction.campaign_name === "Wallet Topup"
-                  ? translate("Wallet") + "\t" + translate("Top Up")
-                  : transaction.campaign_name}
-              </Text>
-            </View>
-            {transaction.iswallet === "1" ? (
-              <WalletIcon
-                fill="#a0a0a0"
-                style={[styles.icon, styles.iconWallet]}
-              />
-            ) : transaction.channel === "snapchat" ? (
-              <SnapGhostIcon width={40} style={[styles.icon]} />
-            ) : (
-              <GoogleSE width={40} style={[styles.icon]} />
+        <View style={styles.header}>
+          {transaction.iswallet === "1" ? (
+            <WalletIcon fill="#FFF" style={[styles.icon]} />
+          ) : transaction.channel === "snapchat" ? (
+            <SnapGhostIcon width={35} height={35} />
+          ) : (
+            <GoogleSE width={35} height={35} />
+          )}
+          <Text
+            numberOfLines={2}
+            uppercase
+            style={[
+              styles.titleText,
+              transaction.campaign_name !== "Wallet Topup" &&
+              !isStringArabic(transaction.campaign_name)
+                ? {
+                    fontFamily: "montserrat-bold-english"
+                  }
+                : {}
+            ]}
+          >
+            {transaction.campaign_name === "Wallet Topup"
+              ? translate("Wallet") + "\t" + translate("Top Up")
+              : transaction.campaign_name}
+          </Text>
+        </View>
+        <View style={styles.mainView}>
+          <View>
+            {(transaction.payment_type === "KNET" ||
+              transaction.payment_type === "Wallet") && (
+              <View>
+                <Text uppercase style={styles.text}>
+                  {translate("Payment Method")}
+                </Text>
+                <Text uppercase style={styles.subText}>
+                  {transaction.payment_type}
+                </Text>
+              </View>
+            )}
+            {transaction.payment_type === "MASTERCARD" && (
+              <View>
+                <Text uppercase style={styles.text}>
+                  {transaction.payment_type}
+                </Text>
+                <Text uppercase style={styles.subText}>
+                  xxxx xxxx xxxx {transaction.card_ending_with}
+                </Text>
+              </View>
             )}
 
-            <Text style={[styles.text]}>{translate("Card Type")}</Text>
-            <Text style={[styles.subText]}>
-              {transaction.payment_type}
-              {transaction.payment_type === "MASTERCARD" &&
-                " " + transaction.card_ending_with}
+            <Text uppercase style={[styles.text, styles.transactionText]}>
+              {translate("Transaction ID")}
             </Text>
-            <Text style={[styles.text]}>{translate("Transaction ID")}</Text>
             <Text style={[styles.subText]}>{transaction.reference_id}</Text>
-            <View style={styles.mainView}>
-              <View style={styles.amountContainer}>
-                <Text style={[styles.text]}>{translate("Amount")}</Text>
-                <Text style={[globalStyles.numbers, styles.amountText]}>
-                  {transaction.total_amount}$
-                </Text>
-              </View>
-              <View pointerEvents="none" style={[styles.containerStyle]}>
-                <Text style={[styles.dateText]}>
-                  {transaction.payment_date.split(" ")[0] +
-                    "\n   " +
-                    transaction.payment_date.split(" ")[1]}
-                </Text>
-              </View>
-            </View>
+          </View>
+          <View style={styles.amountContainer}>
+            <Text uppercase style={[styles.amountTextTitle]}>
+              {translate("Amount")}
+            </Text>
+            <Text style={[globalStyles.numbers, styles.amountText]}>
+              {transaction.total_amount}$
+            </Text>
           </View>
         </View>
+
+        <Text style={[styles.dateText]}>{transaction.payment_date}</Text>
       </View>
     );
   }
