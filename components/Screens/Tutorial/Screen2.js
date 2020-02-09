08648cf40add4acd362a5d5a0b26328095d77d26
saadiyakazi <saadiya@optimizekw.com>
2020-02-09 11:57:25 +0300

    Update Tutorial Screen as per XD

---
+++ components/Screens/Tutorial/Screen2.js
@@ -0,0 +1,161 @@
+import React from "react";
+import { View, Image, Text, I18nManager, Dimensions } from "react-native";
+import { Icon } from "native-base";
+import * as Animatable from "react-native-animatable";
+
+import PenIcon from "../../../assets/SVGs/Pen";
+import EditCameraIcon from "../../../assets/SVGs/CameraCircleOutline";
+
+import styles from "./styles";
+import GradientButton from "../../MiniComponents/GradientButton";
+
+export default class Screen2 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      animateMedia: false,
+      animateSwipe: false
+    };
+  }
+  componentDidUpdate() {
+    const { id, activeSlide, changed } = this.props;
+    if (changed && id === activeSlide) {
+      this.bsnView
+        .animate({
+          0: { scale: 1, translateX: 0 },
+          0.5: {
+            scale: 1.5,
+            translateX: I18nManager.isRTL ? -30 : 30
+          },
+          1: { scale: 1, translateX: 0 }
+        })
+        .then(() => {
+          this.mediaBtnRef
+            .animate({
+              0: {
+                scale: 1,
+                translateX: 0
+              },
+              0.5: {
+                scale: 1.5,
+                translateX: 0
+              },
+              1: {
+                scale: 1,
+                translateX: 0
+              }
+            })
+            .then(() => {
+              this.swipeViewRef.animate({
+                0: {
+                  scale: 1,
+                  translateX: 0
+                },
+                0.5: {
+                  scale: 1.25,
+                  translateX: 0
+                },
+                1: {
+                  scale: 1,
+                  translateX: 0
+                }
+              });
+            });
+        });
+    }
+  }
+
+  render() {
+    const { translate } = this.props.screenProps;
+
+    return (
+      <Animatable.View style={styles.screen2OuterView}>
+        <View
+          style={{
+            borderRadius: 30,
+            position: "absolute",
+            height: "100%",
+            zIndex: -2
+          }}
+        >
+          <Image
+            style={{
+              borderRadius: 30,
+              opacity: 0.4,
+              overflow: "hidden",
+              height: "100%"
+            }}
+            source={require("../../../assets/images/AdImagePlaceholder.png")}
+          />
+        </View>
+
+        <Animatable.View
+          ref={ref => (this.bsnView = ref)}
+          duration={2000}
+          style={styles.bsnView}
+        >
+          <GradientButton
+            style={styles.block1}
+            text={1}
+            textStyle={styles.blockText}
+          />
+          <Animatable.View style={styles.adDesignTopView}>
+            <PenIcon width={17} height={17} />
+            <View style={styles.adDesignDetailView}>
+              <Text style={styles.adDesignHeading}>
+                {translate("Business Name")}
+              </Text>
+              <Text style={styles.adDesignDescp}>ABYAT</Text>
+            </View>
+          </Animatable.View>
+          <Animatable.View style={styles.adDesignTopView}>
+            <PenIcon width={17} height={17} />
+            <View style={styles.adDesignDetailView}>
+              <Text style={styles.adDesignHeading}>
+                {translate("Headline")}
+              </Text>
+              <Text style={styles.adDesignDescp}>New Collection</Text>
+            </View>
+          </Animatable.View>
+        </Animatable.View>
+
+        <Animatable.View
+          ref={ref => (this.mediaBtnRef = ref)}
+          style={styles.mediaButtonView}
+          duration={2000}
+        >
+          <GradientButton
+            style={styles.block}
+            text={2}
+            textStyle={styles.blockText}
+          />
+
+          <EditCameraIcon width={70} height={70} />
+          <Text style={styles.editText}>{translate("Edit Media")}</Text>
+        </Animatable.View>
+        <Animatable.View
+          ref={ref => (this.swipeViewRef = ref)}
+          duration={2000}
+          style={styles.swipeUpView}
+        >
+          <GradientButton
+            style={styles.block3}
+            textStyle={styles.blockText}
+            text={3}
+          />
+
+          <Text style={styles.adDesignWebsite}>{translate("Website")}</Text>
+          <Text style={styles.adDesignWebsitePlaceholder}>
+            www.optimizeapp.com
+          </Text>
+          <Icon
+            type="MaterialIcons"
+            name="arrow-drop-down"
+            fontSize={50}
+            style={styles.downIcon}
+          />
+        </Animatable.View>
+      </Animatable.View>
+    );
+  }
+}
--- components/Screens/Tutorial/index.js
+++ components/Screens/Tutorial/index.js
@@ -1,28 +1,25 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Image, AsyncStorage } from "react-native";
-import { SafeAreaView, NavigationEvents } from "react-navigation";
-import { Container } from "native-base";
+import { AsyncStorage, View, Text, Dimensions, Animated } from "react-native";
+import { SafeAreaView } from "react-navigation";
 import * as Segment from "expo-analytics-segment";
 import { LinearGradient } from "expo-linear-gradient";
 import Swiper from "../../MiniComponents/Swiper";
 import GradientButton from "../../MiniComponents/GradientButton";
 import LoadingScreen from "../../MiniComponents/LoadingScreen";
+import LowerButton from "../../MiniComponents/LowerButton";
+import Screen1 from "./Screen1";
+import Screen2 from "./Screen2";
+import Screen3 from "./Screen3";
+import Screen4 from "./Screen4";
 
 // Style
 import styles from "./styles";
 import { colors } from "../../GradiantColors/colors";
 
-// Icons
-import Background from "../../../assets/SVGs/Background";
-
 //Functions
 import isNull from "lodash/isNull";
-import {
-  widthPercentageToDP,
-  heightPercentageToDP
-} from "react-native-responsive-screen";
-import globalStyles from "../../../GlobalStyles";
+import { globalColors } from "../../../GlobalStyles";
 import segmentEventTrack from "../../segmentEventTrack";
 
 class Tutorial extends Component {
@@ -30,7 +27,34 @@ class Tutorial extends Component {
     header: null
   };
   state = {
-    tutorialOpened: null
+    tutorialOpened: null,
+    activeSlide: 0,
+    slidesData: [
+      {
+        id: 0,
+        heading: "MULTICHANNEL ADVERTISING",
+        description:
+          "Promote your business and reach your target audience whether they're surfing the web, or Snapchatting their friends"
+      },
+      {
+        id: 1,
+        heading: "EASY PUBLISHING",
+        description:
+          "We make it easy so you can focus on growing your business Let us do the heavy lifting for you"
+      },
+      {
+        id: 2,
+        heading: "REPORTS",
+        description:
+          "Compare Key Performance Indicators from previous campaigns Did the changes result in more clicks?"
+      },
+      {
+        id: 3,
+        heading: "NO AGENCIES ALL YOU",
+        description:
+          "Because campaign creation is automated, you get to cut the hefty agency costs out of the equation"
+      }
+    ]
   };
   componentDidMount() {
     AsyncStorage.getItem("tutorialOpened")
@@ -60,53 +84,85 @@ class Tutorial extends Component {
       });
   }
 
+  onSlideChange = e => {
+    //Source: https://stackoverflow.com/questions/43370807/react-native-get-current-page-in-flatlist-when-using-pagingenabled
+    let contentOffset = e.nativeEvent.contentOffset;
+    let viewSize = e.nativeEvent.layoutMeasurement;
+
+    // Divide the horizontal offset by the width of the view to see which page is visible
+    let pageNum = Math.abs(Math.floor(contentOffset.x / viewSize.width));
+    // console.log("scrolled to page ", pageNum);
+    this.setState({
+      activeSlide: pageNum,
+      changed: pageNum !== this.state.activeSlide
+    });
+  };
+  Slide = ({ key, id, heading, description, component }) => {
+    const { translate } = this.props.screenProps;
+    const { activeSlide } = this.state;
+    let screen = "";
+    if (id === activeSlide) {
+      Segment.screen(`Tutorial ${activeSlide + 1}`);
+    }
+
+    if (id === 0)
+      screen = (
+        <Screen1
+          screenProps={this.props.screenProps}
+          navigation={this.props.navigation}
+          id={id}
+          activeSlide={this.state.activeSlide}
+          changed={this.state.changed}
+        />
+      );
+    else if (id === 1)
+      screen = (
+        <Screen2
+          screenProps={this.props.screenProps}
+          navigation={this.props.navigation}
+          id={id}
+          activeSlide={this.state.activeSlide}
+          changed={this.state.changed}
+        />
+      );
+    else if (id === 2)
+      screen = (
+        <Screen3
+          screenProps={this.props.screenProps}
+          navigation={this.props.navigation}
+          id={id}
+          activeSlide={this.state.activeSlide}
+          changed={this.state.changed}
+        />
+      );
+    else if (id === 3)
+      screen = (
+        <Screen4
+          screenProps={this.props.screenProps}
+          navigation={this.props.navigation}
+          id={id}
+          activeSlide={this.state.activeSlide}
+          changed={this.state.changed}
+        />
+      );
+
+    return (
+      <View key={key} style={styles.mainView}>
+        {screen}
+        <View style={styles.blockDescription}>
+          <Text style={styles.heading}>{translate(heading)}</Text>
+          <Text style={styles.description}>{translate(description)}</Text>
+        </View>
+      </View>
+    );
+  };
+
   render() {
     const { translate } = this.props.screenProps;
-    // console.log('trasnslate', translate);
 
-    const Slide = ({ url, i }) => {
-      Segment.screen(`Tutorial Slide ${i}`);
-
-      return (
-        <>
-          <Image
-            style={[
-              styles.imageSlide,
-              {
-                bottom: i === 3 ? heightPercentageToDP("-2%") : 0
-              }
-            ]}
-            source={url}
-            resizeMode="contain"
-          />
-          {i === 3 && (
-            <GradientButton
-              style={[styles.getStartedButton]}
-              onPressAction={() => {
-                segmentEventTrack("Button Get Started Clicked");
-                AsyncStorage.getItem("tutorialOpened")
-                  .then(value => {
-                    if (value == null) {
-                      AsyncStorage.setItem("tutorialOpened", "false");
-                    } else {
-                      AsyncStorage.setItem("tutorialOpened", "true").then(
-                        this.props.navigation.replace("Signin")
-                      );
-                    }
-                  })
-                  .catch(err => console.log(err));
-              }}
-              textStyle={styles.getStartedText}
-              text={translate("Get Started!")}
-            />
-          )}
-        </>
-      );
-    };
     if (isNull(this.state.tutorialOpened)) {
       return <LoadingScreen dash={true} />;
     } else {
-      Segment.screen("Tutorial");
       return (
         <SafeAreaView
           style={styles.safeAreaViewContainer}
@@ -122,31 +178,102 @@ class Tutorial extends Component {
             locations={[1, 0.3]}
             style={styles.gradient}
           />
-          <Container style={styles.container}>
-            <Swiper
-              backgroundColor={["#0000", "#0000", "#0000", "#0000"]}
-              dots
-              dotsColor="rgba(255, 255, 255, 0.25)"
-              dotsColorActive="rgba(255, 255, 255, 1)"
-            >
-              <Slide
-                url={require("../../../assets/images/tutorial/tutorial-1.png")}
-                i={1}
+
+          <Swiper
+            backgroundColor={["#0000", "#0000", "#0000", "#0000"]}
+            dots
+            dotsColor="rgba(255, 255, 255, 0.25)"
+            dotsColorActive={globalColors.orange}
+            onSlideChange={this.onSlideChange}
+            ref={ref => (this.swiperRef = ref)}
+          >
+            {this.state.slidesData.map(item =>
+              this.Slide({
+                key: item.id,
+                ...item
+              })
+            )}
+          </Swiper>
+          <View style={styles.bottomView}>
+            {this.state.activeSlide === 3 ? (
+              <GradientButton
+                style={[styles.getStartedButton]}
+                onPressAction={() => {
+                  segmentEventTrack("Button Get Started Clicked");
+                  AsyncStorage.getItem("tutorialOpened")
+                    .then(value => {
+                      if (value == null) {
+                        AsyncStorage.setItem("tutorialOpened", "false");
+                      } else {
+                        AsyncStorage.setItem("tutorialOpened", "true").then(
+                          this.props.navigation.replace("Signin")
+                        );
+                      }
+                    })
+                    .catch(err => console.log(err));
+                }}
+                textStyle={styles.getStartedText}
+                text={translate("Get Started!")}
+                uppercase
               />
-              <Slide
-                url={require("../../../assets/images/tutorial/tutorial-2.png")}
-                i={2}
-              />
-              <Slide
-                url={require("../../../assets/images/tutorial/tutorial-3.png")}
-                i={3}
-              />
-              {/* <Slide
-                url={require("../../../assets/images/tutorial/tutorial-4.png")}
-                i={4}
-              /> */}
-            </Swiper>
-          </Container>
+            ) : (
+              <>
+                <GradientButton
+                  transparent
+                  text={translate("SKIP")}
+                  style={styles.skipButton}
+                  onPressAction={() => {
+                    segmentEventTrack(
+                      `Button SKIP Clicked after screen ${this.state
+                        .activeSlide + 1}`
+                    );
+
+                    AsyncStorage.getItem("tutorialOpened")
+                      .then(value => {
+                        if (value == null) {
+                          AsyncStorage.setItem("tutorialOpened", "false");
+                        } else {
+                          AsyncStorage.setItem("tutorialOpened", "true").then(
+                            this.props.navigation.replace("Signin")
+                          );
+                        }
+                      })
+                      .catch(err => console.log(err));
+                  }}
+                />
+                <LowerButton
+                  function={() => {
+                    // SOURCE: https://stackoverflow.com/questions/55580525/react-native-scrollview-scrollto-spring-animation
+                    const animatedValue = new Animated.Value(0);
+                    const id = animatedValue.addListener(({ value }) => {
+                      this.swiperRef.getNode().scrollTo({
+                        x: value,
+                        y: 0,
+                        animated: false
+                      });
+                    });
+                    Animated.spring(animatedValue, {
+                      toValue:
+                        (this.state.activeSlide + 1) *
+                        Dimensions.get("window").width
+                    }).start(() => {
+                      animatedValue.removeListener(id); /* finished callback */
+                    });
+
+                    this.setState({
+                      activeSlide: this.state.activeSlide + 1,
+                      changed:
+                        ((this.state.activeSlide + 1) *
+                          Dimensions.get("window").width) /
+                          Dimensions.get("window").width !==
+                        this.state.activeSlide
+                    });
+                  }}
+                  style={styles.lowerBtn}
+                />
+              </>
+            )}
+          </View>
         </SafeAreaView>
       );
     }
