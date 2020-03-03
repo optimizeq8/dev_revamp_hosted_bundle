86858ef2d040b49c4e06ea601b776fdc6dbfd2b2
SamyOptimize <samy@optimizekw.com>
2020-03-03 16:31:27 +0300

    Update version numbers

--- components/Screens/Menu/index.js
+++ components/Screens/Menu/index.js
@@ -348,7 +348,7 @@ class Menu extends Component {
               </TouchableOpacity>
               <Text style={styles.version}>
                 {translate("Version:")}
-                {Constants.manifest.version}/6/
+                {Constants.manifest.version}/1/
                 {Constants.manifest.ios.buildNumber}/
                 {Constants.manifest.android.versionCode}
               </Text>
