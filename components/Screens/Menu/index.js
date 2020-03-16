563d6c2330a5257b6226f6ea0382e77030f3b141
SamyOptimize <samy@optimizekw.com>
2020-03-16 17:21:48 +0300

    bump dev ota versions to 11

--- components/Screens/Menu/index.js
+++ components/Screens/Menu/index.js
@@ -347,7 +347,7 @@ class Menu extends Component {
               </TouchableOpacity>
               <Text style={styles.version}>
                 {translate("Version:")}
-                {Constants.manifest.version}/10/
+                {Constants.manifest.version}/11/
                 {Constants.manifest.ios.buildNumber}/
                 {Constants.manifest.android.versionCode}/
                 {Constants.manifest.releaseChannel}/
