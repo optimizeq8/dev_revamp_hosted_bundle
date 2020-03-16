fa772f353d40fd115ecb4b34b65d29e4fc6ceefe
SamyOptimize <samy@optimizekw.com>
2020-03-16 23:03:03 +0300

    Took out stickers

--- android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
@@ -14,7 +14,7 @@ public class AppConstants {
   public static final String VERSION_NAME = "0.2.210";
   public static String INITIAL_URL = "exp://exp.host/@samyoptimize/optimize_app";
   public static final String SHELL_APP_SCHEME = "optimize";
-  public static final String RELEASE_CHANNEL = "development";
+  public static final String RELEASE_CHANNEL = "prod-v3.1";
   public static boolean SHOW_LOADING_VIEW_IN_SHELL_APP = true;
   public static boolean ARE_REMOTE_UPDATES_ENABLED = true;
   public static final List<Constants.EmbeddedResponse> EMBEDDED_RESPONSES;
@@ -58,10 +58,11 @@ public class AppConstants {
         
         
         
+        
         // ADD EMBEDDED RESPONSES HERE
         // START EMBEDDED RESPONSES
         embeddedResponses.add(new Constants.EmbeddedResponse("https://exp.host/@samyoptimize/optimize_app", "assets://shell-app-manifest.json", "application/json"));
-        embeddedResponses.add(new Constants.EmbeddedResponse("https://d1wp6m56sqw74a.cloudfront.net/%40samyoptimize%2Foptimize_app%2F0.3.10%2F96cf9047f97664d012f7fa8fcfe87150-36.0.0-android.js", "assets://shell-app.bundle", "application/javascript"));
+        embeddedResponses.add(new Constants.EmbeddedResponse("https://d1wp6m56sqw74a.cloudfront.net/%40samyoptimize%2Foptimize_app%2F0.3.21%2F2f56828885069461d325c26b782043d7-36.0.0-android.js", "assets://shell-app.bundle", "application/javascript"));
         // END EMBEDDED RESPONSES
     EMBEDDED_RESPONSES = embeddedResponses;
   }
--- components/Functions/PhotoEditorConfiguration.js
+++ components/Functions/PhotoEditorConfiguration.js
@@ -21,45 +21,45 @@ export default (custom = {}) => {
       personalStickers: true,
       defaultPersonalStickerTintMode: TintMode.COLORIZED,
       categories: [
-        { identifier: "imgly_sticker_category_emoticons" },
-        { identifier: "imgly_sticker_category_shapes" },
-        {
-          identifier: "optimize_sticker_category",
-          name: "Logos",
-          thumbnailURI: "https://i.imgur.com/PVRl4zu.png",
-          items: [
-            {
-              identifier: "optimize_sticker_logo",
-              name: "Optimize Logo",
-              stickerURI: "https://i.imgur.com/PVRl4zu.png"
-            },
-            {
-              identifier: "optimize_sticker_icon",
-              name: "Optimize Icon",
-              stickerURI: "https://i.imgur.com/rjo4oOd.png"
-            },
-            {
-              identifier: "optimize_team",
-              name: "Optimize Team",
-              stickerURI: "https://i.imgur.com/Awl2WJo.png"
-            },
-            {
-              identifier: "optimize_graph",
-              name: "Graph",
-              stickerURI: "https://i.imgur.com/Uinv5o9.png"
-            },
-            {
-              identifier: "optimize_purple_logo",
-              name: "Optimize purple logo",
-              stickerURI: "https://i.imgur.com/KtlUCVh.png"
-            },
-            {
-              identifier: "optimize_o_logo",
-              name: "Optimize name logo",
-              stickerURI: "https://i.imgur.com/bK7zCxW.png"
-            }
-          ]
-        }
+        { identifier: "imgly_sticker_category_shapes" }
+        // { identifier: "imgly_sticker_category_emoticons" },
+        // {
+        //   identifier: "optimize_sticker_category",
+        //   name: "Logos",
+        //   thumbnailURI: "https://i.imgur.com/PVRl4zu.png",
+        //   items: [
+        //     {
+        //       identifier: "optimize_sticker_logo",
+        //       name: "Optimize Logo",
+        //       stickerURI: "https://i.imgur.com/PVRl4zu.png"
+        //     },
+        //     {
+        //       identifier: "optimize_sticker_icon",
+        //       name: "Optimize Icon",
+        //       stickerURI: "https://i.imgur.com/rjo4oOd.png"
+        //     },
+        //     {
+        //       identifier: "optimize_team",
+        //       name: "Optimize Team",
+        //       stickerURI: "https://i.imgur.com/Awl2WJo.png"
+        //     },
+        //     {
+        //       identifier: "optimize_graph",
+        //       name: "Graph",
+        //       stickerURI: "https://i.imgur.com/Uinv5o9.png"
+        //     },
+        //     {
+        //       identifier: "optimize_purple_logo",
+        //       name: "Optimize purple logo",
+        //       stickerURI: "https://i.imgur.com/KtlUCVh.png"
+        //     },
+        //     {
+        //       identifier: "optimize_o_logo",
+        //       name: "Optimize name logo",
+        //       stickerURI: "https://i.imgur.com/bK7zCxW.png"
+        //     }
+        //   ]
+        // }
       ]
     }
   };
