b04118657a0e493d1088ec514c951d3e361a5329
SamyOptimize <samy@optimizekw.com>
2020-03-18 12:42:52 +0300

    bump live ota version to 13

--- components/Screens/Menu/index.js
+++ components/Screens/Menu/index.js
@@ -347,7 +347,7 @@ class Menu extends Component {
               </TouchableOpacity>
               <Text style={styles.version}>
                 {translate("Version:")}
-                {Constants.manifest.version}/12/
+                {Constants.manifest.version}/13/
                 {Constants.manifest.ios.buildNumber}
                 {/* {Constants.manifest.android.versionCode}/ */}
               </Text>
