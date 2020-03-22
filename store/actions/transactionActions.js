067eda568459dc43551d705b124381cee17b0629
SamyOptimize <samy@optimizekw.com>
2020-03-22 18:54:25 +0300

    Add key channel: instagram to wallet payment

--- store/actions/transactionActions.js
+++ store/actions/transactionActions.js
@@ -209,6 +209,8 @@ export const useWallet = (campaign_id, setWalletModal, retries = 3) => {
     var info = { campaign_id: campaign_id };
     if (getState().transA.channel === "google")
       info = { ...info, channel: getState().transA.channel };
+    else if (getState().transA.channel === "instagram")
+      info = { ...info, channel: getState().transA.channel };
     createBaseUrl()
       .post(`useWallet`, info, { timeout: 10000 })
       .then(res => {
@@ -333,6 +335,8 @@ export const checkoutwithWallet = (campaign_id, retries = 3) => {
     });
     var info = { campaign_id: campaign_id };
     if (getState().transA.channel === "google")
+      info = { ...info, channel: getState().transA.channel };
+    if (getState().transA.channel === "instagram")
       info = { ...info, channel: getState().transA.channel };
 
     createBaseUrl()
