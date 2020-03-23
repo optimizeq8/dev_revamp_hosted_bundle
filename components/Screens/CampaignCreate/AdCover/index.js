9188c4f47f61861add8a36e301cc9e7526e8b971
SamyOptimize <samy@optimizekw.com>
2020-03-23 13:15:31 +0300

    Modify photoeditor to export images as png

--- components/Screens/CampaignCreate/AdCover/index.js
+++ components/Screens/CampaignCreate/AdCover/index.js
@@ -42,6 +42,7 @@ import segmentEventTrack from "../../../segmentEventTrack";
 import { PESDK, Configuration } from "react-native-photoeditorsdk";
 import PhotoEditorConfiguration from "../../../Functions/PhotoEditorConfiguration";
 import MediaModal from "./MediaModal";
+import { SaveFormat } from "expo-image-manipulator";
 
 class AdCover extends Component {
   static navigationOptions = {
@@ -274,7 +275,9 @@ class AdCover extends Component {
             serialization = manipResult.serialization;
             if (logo.height !== 284 && logo.width !== 993)
               manipResult = await ImageManipulator.manipulateAsync(
-                manipResult.image
+                manipResult.image,
+                [],
+                { format: SaveFormat.PNG }
               );
 
             if (
@@ -300,7 +303,7 @@ class AdCover extends Component {
               ],
               {
                 compress: 1,
-                format: "png"
+                format: SaveFormat.PNG
               }
             );
           }
@@ -706,7 +709,6 @@ class AdCover extends Component {
   };
   render() {
     let { cover, coverHeadlineError, logoSerialization } = this.state;
-    console.log(logoSerialization);
 
     let { coverHeadline, logo } = this.state.campaignInfo;
     const { translate } = this.props.screenProps;
