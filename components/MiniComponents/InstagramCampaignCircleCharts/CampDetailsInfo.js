cfe9f2c0a6d2be656567d9529721fbd9bcfa3808
saadiyakazi <saadiya@optimizekw.com>
2020-03-24 22:36:52 +0300

    Fix video upload issue and add instgram card to dashboard

---
+++ components/MiniComponents/InstagramCampaignCircleCharts/CampDetailsInfo.js
@@ -0,0 +1,82 @@
+import React from "react";
+import PlaceholderLine from "../PlaceholderLine";
+import { View } from "react-native";
+import styles from "./styles";
+import ProgressBar from "../ProgressBar";
+import dateFormat from "dateformat";
+import { widthPercentageToDP } from "react-native-responsive-screen";
+import { Text } from "native-base";
+import TimeDifferance from "../../Functions/TimeDifferance";
+import { globalColors } from "../../../GlobalStyles";
+import isNaN from "lodash/isNaN";
+export default props => {
+  let { campaign, loading, screenProps } = props;
+  let { translate } = screenProps;
+  let statusOfCampaign = campaign
+    ? new Date().setHours(0, 0, 0, 0) <
+      new Date(campaign.start_time).setHours(0, 0, 0, 0)
+      ? "starts"
+      : new Date().setHours(0, 0, 0, 0) <
+        new Date(campaign.end_time).setHours(0, 0, 0, 0)
+      ? "ends"
+      : "ended"
+    : "";
+  let currentDate = new Date().toLocaleDateString();
+  return (
+    <View style={{ alignSelf: "center", top: 10 }}>
+      {loading ? (
+        <PlaceholderLine width={widthPercentageToDP(85)} />
+      ) : (
+        <View>
+          {statusOfCampaign !== "starts" &&
+            (new Date().setHours(0, 0, 0, 0) <=
+              new Date(campaign.end_time).setHours(0, 0, 0, 0) &&
+            new Date().setHours(0, 0, 0, 0) >=
+              new Date(campaign.start_time).setHours(0, 0, 0, 0) &&
+            campaign.campaign_end === "0" ? (
+              <Text style={[styles.subtext]}>
+                {TimeDifferance(currentDate, campaign.end_time) === 0
+                  ? 1
+                  : TimeDifferance(currentDate, campaign.end_time)}{" "}
+                {translate("Day(s) left")}
+              </Text>
+            ) : (
+              <Text style={styles.subtext}>{translate("Campaign ended")}</Text>
+            ))}
+          <ProgressBar
+            color={globalColors.orange}
+            progress={
+              statusOfCampaign === "starts"
+                ? 0
+                : TimeDifferance(campaign.start_time, currentDate) /
+                  (TimeDifferance(campaign.start_time, campaign.end_time) === 0
+                    ? 1
+                    : TimeDifferance(campaign.start_time, campaign.end_time))
+            }
+            borderWidth={0}
+            unfilledColor="#0004"
+            height={25}
+            borderRadius={30}
+            width={widthPercentageToDP(90)}
+          />
+          {statusOfCampaign !== "ended" && (
+            <Text style={[styles.chartSubtext, { alignSelf: "flex-start" }]}>
+              {translate(`Campaign {{statusOfCampaign}} on`, {
+                statusOfCampaign: statusOfCampaign
+              }) +
+                " " +
+                dateFormat(
+                  new Date(
+                    statusOfCampaign === "starts"
+                      ? campaign.start_time.split("T")[0].replace(/-/gi, "/")
+                      : campaign.end_time.split("T")[0].replace(/-/gi, "/")
+                  ),
+                  "mmm dS, yyyy"
+                )}
+            </Text>
+          )}
+        </View>
+      )}
+    </View>
+  );
+};
---
+++ components/MiniComponents/InstagramCampaignCircleCharts/styles.js
@@ -0,0 +1,106 @@
+import { StyleSheet, PixelRatio } from "react-native";
+
+import { widthPercentageToDP } from "react-native-responsive-screen";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp
+} from "react-native-responsive-screen";
+import { globalColors } from "../../../GlobalStyles";
+
+export const colors = {
+  black: "#1a1917",
+  gray: "#888888",
+  background1: "#751AFF",
+  background2: "#751AFF"
+};
+const styles = StyleSheet.create({
+  campaignInfoStyle: {},
+  titleContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignSelf: "flex-start",
+    alignItems: "center",
+    width: "100%",
+    height: 50
+  },
+  title: {
+    fontFamily: "montserrat-bold",
+    fontSize: 16,
+    color: globalColors.white,
+    alignSelf: "flex-start"
+  },
+  subtext: {
+    fontFamily: "montserrat-regular",
+    fontSize: 12,
+    color: "#fff",
+    textTransform: "uppercase",
+    textAlign: "left"
+  },
+  subtextDetail: {
+    fontFamily: "montserrat-bold"
+  },
+  chart: {
+    alignItems: "center",
+    paddingHorizontal: 5
+  },
+  chartText: {
+    color: "#fff",
+    fontFamily: "montserrat-medium",
+    fontSize: 16,
+    marginLeft: 10
+  },
+  chartSubtext: {
+    alignSelf: "center",
+    paddingTop: 10,
+    fontFamily: "montserrat-regular",
+    fontSize: 12,
+    color: "#fff",
+    textAlign: "left"
+  },
+  campaignIcons: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#0004",
+    marginVertical: 2.5,
+    borderRadius: 20,
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    width: 160,
+    height: 50
+  },
+  campaignNumbers: {
+    paddingHorizontal: 0,
+    color: "#FF9D00",
+    fontFamily: "montserrat-bold-english",
+    fontSize: 15 / PixelRatio.getFontScale(),
+    textAlign: "left"
+    // paddingHorizontal: 10
+  },
+  campaignNumbersDetail: {
+    fontFamily: "montserrat-regular-english",
+    fontSize: 16 / PixelRatio.getFontScale()
+  },
+  campaignInfo: {
+    flexDirection: "column",
+    paddingHorizontal: 10
+    // alignItems: "center"
+    // width: wp(40),
+    // left: 10
+  },
+  campaignInfoCard: {
+    flexDirection: "row",
+    top: 10,
+    flex: 3
+  },
+  campaignCard: {
+    flexDirection: "row",
+    alignItems: "center"
+    // paddingHorizontal: 10
+  },
+  adPerformanceLowerBUtton: {
+    width: 35,
+    height: 35
+  }
+});
+
+export default styles;
