cb339285c622dd9330c0015a8873b833504af6e1
saadiyakazi <saadiya@optimizekw.com>
2020-04-07 15:28:00 +0300

    Bump OTA to 20 for sprint_v1

--- components/Screens/Menu/index.js
+++ components/Screens/Menu/index.js
@@ -347,7 +347,7 @@ class Menu extends Component {
               </TouchableOpacity>
               <Text style={styles.version}>
                 {translate("Version:")}
-                {Constants.manifest.version}/19/
+                {Constants.manifest.version}/20/
                 {Constants.manifest.ios.buildNumber}/
                 {Constants.manifest.android.versionCode}/
                 {Constants.manifest.releaseChannel}/
