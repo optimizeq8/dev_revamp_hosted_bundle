3c0bb7d5150c5daa5b395a8a93c6ceb827f61da1
saadiyakazi <saadiya@optimizekw.com>
2020-04-20 17:53:50 +0300

    Bump OTA to 47

--- components/Screens/Menu/index.js
+++ components/Screens/Menu/index.js
@@ -375,7 +375,7 @@ class Menu extends Component {
               </TouchableOpacity>
               <Text style={styles.version}>
                 {translate("Version:")}
-                {Constants.manifest.version}/46/
+                {Constants.manifest.version}/47/
                 {Constants.manifest.ios.buildNumber}/
                 {Constants.manifest.android.versionCode}/
                 {Constants.manifest.releaseChannel}/
