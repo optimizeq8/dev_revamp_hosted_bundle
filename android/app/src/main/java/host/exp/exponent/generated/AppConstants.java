141b6d3b8049e14e9c8844401e6e77d9afadf0ef
saadiyakazi <saadiya@optimizekw.com>
2020-05-17 21:36:55 +0300

    OTA update 53

--- android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
@@ -81,10 +81,11 @@ public class AppConstants {
       
       
       
+      
       // ADD EMBEDDED RESPONSES HERE
       // START EMBEDDED RESPONSES
       embeddedResponses.add(new Constants.EmbeddedResponse("https://exp.host/@samyoptimize/optimize_app", "assets://shell-app-manifest.json", "application/json"));
-      embeddedResponses.add(new Constants.EmbeddedResponse("https://d1wp6m56sqw74a.cloudfront.net/%40samyoptimize%2Foptimize_app%2F0.3.200%2F2153ba996016ad1da8f472fdad2f2254-36.0.0-android.js", "assets://shell-app.bundle", "application/javascript"));
+      embeddedResponses.add(new Constants.EmbeddedResponse("https://d1wp6m56sqw74a.cloudfront.net/%40samyoptimize%2Foptimize_app%2F0.3.200%2Fd7b9cb94f498a354f4e07a17e7164e38-36.0.0-android.js", "assets://shell-app.bundle", "application/javascript"));
       // END EMBEDDED RESPONSES
     EMBEDDED_RESPONSES = embeddedResponses;
   }
