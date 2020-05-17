81a8dd4d7ec5a2710a948712a029c8cf28245cb5
SamyOptimize <samy@optimizekw.com>
2020-05-17 13:11:15 +0300

    Fix autolink issue on ios for react-native-screens

--- react-native.config.js
+++ react-native.config.js
@@ -2,41 +2,46 @@ module.exports = {
   dependencies: {
     // These libraries are included in ExpoKit; autolinking them
     // results in duplicate symbol errors
+    "react-native-screens": {
+      platforms: {
+        ios: null,
+      },
+    },
     "react-native-gesture-handler": {
       platforms: {
-        ios: null
-      }
+        ios: null,
+      },
     },
     "react-native-webview": {
       platforms: {
-        ios: null
-      }
+        ios: null,
+      },
     },
     "react-native-svg": {
       platforms: {
-        ios: null
-      }
+        ios: null,
+      },
     },
     "react-native-reanimated": {
       platforms: {
-        ios: null
-      }
+        ios: null,
+      },
     },
     "lottie-react-native": {
       platforms: {
         ios: null,
-        android: null
-      }
+        android: null,
+      },
     },
     "react-native-maps": {
       platforms: {
-        ios: null
-      }
+        ios: null,
+      },
     },
     "react-native-vector-icons": {
       platforms: {
-        ios: null
-      }
-    }
-  }
+        ios: null,
+      },
+    },
+  },
 };
