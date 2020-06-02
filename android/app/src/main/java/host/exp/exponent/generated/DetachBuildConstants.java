3e303b20b8f1c4913c8d40071c274d13f445824b
SamyOptimize <samy@optimizekw.com>
2020-06-02 11:54:46 +0300

    Remove linking for analytics and mixpanel for android

--- android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++
@@ -1,9 +0,0 @@
-package host.exp.exponent.generated;
-
-// This file is auto-generated. Please don't rename!
-public class DetachBuildConstants {
-
-  public static final String DEVELOPMENT_URL = "optimize://192.168.43.104:19000";
-
-
-}
