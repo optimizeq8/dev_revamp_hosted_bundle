9fb2e68f6ee0cd1d6cf35000cffe31f52a65918a
saadiyakazi <saadiya@optimizekw.com>
2020-06-04 18:19:05 +0300

    Ota 77 publish

--- components/Screens/Menu/index.js
+++ components/Screens/Menu/index.js
@@ -442,7 +442,7 @@ class Menu extends Component {
               </TouchableOpacity>
               <Text style={styles.version}>
                 {translate("Version:")}
-                {Constants.nativeAppVersion}/76/
+                {Constants.nativeAppVersion}/77/
                 {Constants.nativeBuildVersion}
               </Text>
             </ScrollView>
