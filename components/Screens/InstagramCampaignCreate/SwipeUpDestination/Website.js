0424c534eaed07859fc1d0c19a2873a15373ad13
saadiyakazi <saadiya@optimizekw.com>
2020-06-04 14:06:43 +0300

    [OA-208] Change input fields as per new design

--- components/Screens/InstagramCampaignCreate/SwipeUpDestination/Website.js
+++ components/Screens/InstagramCampaignCreate/SwipeUpDestination/Website.js
@@ -5,7 +5,7 @@ import {
   BackHandler,
   ScrollView,
   TouchableOpacity,
-  I18nManager
+  I18nManager,
 } from "react-native";
 import { SafeAreaView } from "react-navigation";
 import { Text, Item, Input, Icon, Button } from "native-base";
@@ -14,10 +14,14 @@ import split from "lodash/split";
 import isEmpty from "lodash/isEmpty";
 import * as Segment from "expo-analytics-segment";
 import Picker from "../../../MiniComponents/Picker";
+import ModalField from "../../../MiniComponents/InputFieldNew/ModalField";
+import WebsiteField from "../../../MiniComponents/InputFieldNew/Website";
+
 import LowerButton from "../../../MiniComponents/LowerButton";
 
 //icons
 import WebsiteIcon from "../../../../assets/SVGs/SwipeUps/Website";
+import WindowIcon from "../../../../assets/SVGs/Window";
 
 // Style
 import styles from "../styles/swipeUpDestination.styles";
@@ -34,14 +38,14 @@ import segmentEventTrack from "../../../segmentEventTrack";
 
 class Website extends Component {
   static navigationOptions = {
-    header: null
+    header: null,
   };
   constructor(props) {
     super(props);
     this.state = {
       campaignInfo: {
         link: "",
-        call_to_action: list.InstagramFeedAd[0].call_to_action_list[0]
+        call_to_action: list.InstagramFeedAd[0].call_to_action_list[0],
       },
       callActionLabel: "",
       networkString: netLoc[0].label,
@@ -49,7 +53,8 @@ class Website extends Component {
       callactions:
         list[this.props.data["campaign_type"]][0].call_to_action_list,
       urlError: "",
-      inputCallToAction: false
+      inputCallToAction: false,
+      callToActionError: null,
     };
   }
 
@@ -59,13 +64,11 @@ class Website extends Component {
       this.props.data.hasOwnProperty("link") &&
       this.props.data.link !== ""
     ) {
-      const url = split(this.props.data.link, "://");
       this.setState({
         campaignInfo: {
-          link: url[1],
-          call_to_action: this.props.data.call_to_action
+          link: his.props.data.link,
+          call_to_action: this.props.data.call_to_action,
         },
-        networkString: url[0] + "://"
       });
     }
     BackHandler.addEventListener("hardwareBackPress", this.handleBackButton);
@@ -79,19 +82,16 @@ class Website extends Component {
   }
   validateUrl = () => {
     const { translate } = this.props.screenProps;
-    const urlError = validateWrapper(
-      "url",
-      this.state.networkString + this.state.campaignInfo.link
-    );
+    const urlError = validateWrapper("url", this.state.campaignInfo.link);
     this.setState({
-      urlError
+      urlError,
     });
     if (urlError) {
       showMessage({
         message: translate(urlError),
         type: "warning",
         position: "top",
-        duration: 7000
+        duration: 7000,
       });
       return false;
     } else {
@@ -109,7 +109,7 @@ class Website extends Component {
       this.props.save_campaign_info_instagram({
         ...this.props.data,
         call_to_action: this.state.campaignInfo.call_to_action,
-        link: this.state.networkString + this.state.campaignInfo.link
+        link: this.state.campaignInfo.link,
       });
       // segmentEventTrack("Submitted Website SwipeUp Success", {
       //   campaign_website_url: this.state.campaignInfo.attachment
@@ -117,17 +117,17 @@ class Website extends Component {
       this.props.navigation.navigate(`${this.props.data.campaign_type}Design`);
     }
   };
-  onSelectedCallToActionIdChange = value => {
+  onSelectedCallToActionIdChange = (value) => {
     // NOTE: compulsory to pass this function
     // console.log("businescatId", value);
   };
   closeCallToActionModal = () => {
     this.setState({
-      inputCallToAction: false
+      inputCallToAction: false,
     });
   };
 
-  onSelectedCallToActionChange = value => {
+  onSelectedCallToActionChange = (value) => {
     if (value && !isEmpty(value)) {
       // segmentEventTrack("Selected Website Call to Action", {
       //   campaign_call_to_action: value[0].label
@@ -138,9 +138,9 @@ class Website extends Component {
             ...this.state.campaignInfo,
             call_to_action: {
               label: value[0].label,
-              value: value[0].value
-            }
-          }
+              value: value[0].value,
+            },
+          },
         },
         () => {
           this.closeCallToActionModal();
@@ -148,18 +148,78 @@ class Website extends Component {
       );
     }
   };
+
+  /**
+   * Set website value
+   */
+
+  setWebsiteValue = (value) => {
+    this.setState({
+      campaignInfo: {
+        ...this.state.campaignInfo,
+        link: value,
+      },
+    });
+  };
+  openCallToActionModal = () => {
+    this.setState({ inputCallToAction: true });
+  };
+
+  getValidInfo = (stateError, validObj) => {
+    let state = {};
+    state[stateError] = validObj;
+    this.setState({
+      ...state,
+    });
+  };
   render() {
     const { translate } = this.props.screenProps;
     return (
-      <View style={[styles.websiteContent]}>
-        <WebsiteIcon style={styles.icon} width={60} height={60} />
+      <SafeAreaView style={[styles.websiteContent]}>
+        <WebsiteIcon style={styles.icon} width={60} height={60} fill={"#FFF"} />
         <View style={[styles.textcontainer]}>
           <Text style={styles.titletext}>{translate("Website")}</Text>
           <Text style={styles.subtext}>
             {translate("The user will be taken to your website")}
           </Text>
         </View>
+        <View style={{ paddingHorizontal: 80 }}>
+          <ModalField
+            stateName={"callToAction"}
+            setModalVisible={this.openCallToActionModal}
+            modal={true}
+            label={"call to action"}
+            valueError={this.state.callToActionError}
+            getValidInfo={this.getValidInfo}
+            disabled={false}
+            valueText={this.state.campaignInfo.call_to_action.label}
+            value={this.state.campaignInfo.call_to_action.label}
+            incomplete={false}
+            translate={this.props.screenProps.translate}
+            icon={WindowIcon}
+            isVisible={this.state.inputCallToAction}
+          />
 
+          <WebsiteField
+            stateName={"url"}
+            customStyle={{ paddingHorizontal: 0, height: 50 }}
+            screenProps={this.props.screenProps}
+            website={this.state.campaignInfo.link}
+            setWebsiteValue={this.setWebsiteValue}
+            stateNameError={this.state.urlError}
+            getValidInfo={this.validateUrl}
+            // disabled={
+            //   (this.state.editBusinessInfo &&
+            //     this.props.editBusinessInfoLoading) ||
+            //   this.props.savingRegister
+            // }
+          />
+          <LowerButton
+            checkmark={true}
+            bottom={0}
+            function={this._handleSubmission}
+          />
+        </View>
         <Picker
           showIcon={true}
           screenProps={this.props.screenProps}
@@ -175,166 +235,21 @@ class Website extends Component {
           screenName={"Swipe up destination Website"}
           closeCategoryModal={this.closeCallToActionModal}
         />
-        <View>
-          <View style={[styles.callToActionLabelView]}>
-            <Text uppercase style={[styles.inputLabel]}>
-              {translate("call to action")}
-            </Text>
-          </View>
-          <Item
-            // rounded
-            style={[styles.input, { paddingHorizontal: 30 }]}
-            onPress={() => {
-              // segmentEventTrack(
-              //   "Button Clicked to open Call to action Modal"
-              // );
-              this.setState({ inputCallToAction: true }, () => {
-                // if (this.state.inputCallToAction) {
-                //   Segment.screen("Call to Action Modal");
-                // }
-              });
-            }}
-          >
-            <Text style={styles.callActionLabel}>
-              {this.state.campaignInfo.call_to_action.label}
-            </Text>
-            <Icon
-              type="AntDesign"
-              name="down"
-              style={{ color: "#fff", fontSize: 20, left: 25 }}
-            />
-          </Item>
-        </View>
-
-        <View style={styles.topContainer}>
-          <View style={styles.inputContainer}>
-            <View style={styles.websiteView}>
-              <View style={[styles.websiteLabelView]}>
-                <Text uppercase style={[styles.inputLabel]}>
-                  {translate("Website")}
-                </Text>
-              </View>
-              <Item
-                style={[
-                  styles.input
-                  // this.state.urlError
-                  //     ? GlobalStyles.redBorderColor
-                  //     : GlobalStyles.transparentBorderColor
-                ]}
-              >
-                <TouchableOpacity
-                  style={[
-                    GlobalStyles.orangeBackgroundColor,
-                    {
-                      borderRadius: 30,
-                      width: 54,
-                      height: 54,
-                      alignItems: "center",
-                      justifyContent: "center"
-                    }
-                  ]}
-                  onPress={() => {
-                    if (this.state.networkString === "https://") {
-                      this.setState(
-                        {
-                          networkString: "http://"
-                        }
-                        // () => {
-                        //   segmentEventTrack(
-                        //     "Changed SwipeUp Website network string",
-                        //     {
-                        //       campaign_website_network_string: this
-                        //         .state.networkString
-                        //     }
-                        //   );
-                        // }
-                      );
-                    } else {
-                      this.setState(
-                        {
-                          networkString: "https://"
-                        }
-                        // () => {
-                        //   segmentEventTrack(
-                        //     "Changed SwipeUp Website network string",
-                        //     {
-                        //       campaign_website_network_string: this
-                        //         .state.networkString
-                        //     }
-                        //   );
-                        // }
-                      );
-                    }
-                  }}
-                >
-                  <Text uppercase style={styles.networkLabel}>
-                    {this.state.networkString === "https://" ? "https" : "http"}
-                  </Text>
-                  <Text uppercase style={styles.networkLabel}>
-                    {`< >`}
-                  </Text>
-                </TouchableOpacity>
-                <Input
-                  style={[
-                    styles.inputtext,
-                    I18nManager.isRTL
-                      ? { textAlign: "right" }
-                      : { textAlign: "left" }
-                  ]}
-                  placeholder={translate(`Enter your website's URL`)}
-                  placeholderTextColor={globalColors.white}
-                  value={this.state.campaignInfo.link}
-                  autoCorrect={false}
-                  autoCapitalize="none"
-                  onChangeText={value =>
-                    this.setState({
-                      campaignInfo: {
-                        ...this.state.campaignInfo,
-                        link: value
-                      }
-                    })
-                  }
-                  onBlur={async () => {
-                    // segmentEventTrack(
-                    //   "Changed Website URL attachment",
-                    //   {
-                    //     campaign_website_url_attachment: this.state
-                    //       .campaignInfo.attachment
-                    //   }
-                    // );
-                    const valid = await this.validateUrl();
-                    // if (!valid) {
-                    //   segmentEventTrack("Error blur input Website", {
-                    //     campaign_error_website_url: this.state.urlError
-                    //   });
-                    // }
-                  }}
-                />
-              </Item>
-            </View>
-          </View>
-        </View>
-
-        <LowerButton
-          checkmark={true}
-          bottom={0}
-          function={this._handleSubmission}
-        />
-      </View>
+      </SafeAreaView>
     );
   }
 }
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   data: state.instagramAds.data,
-  adType: state.instagramAds.adType
+  adType: state.instagramAds.adType,
 });
-const mapDispatchToProps = dispatch => ({
-  save_campaign_info_instagram: info =>
+const mapDispatchToProps = (dispatch) => ({
+  save_campaign_info_instagram: (info) =>
     dispatch(actionCreators.save_campaign_info_instagram(info)),
 
-  saveCampaignSteps: step =>
-    dispatch(actionCreators.saveCampaignStepsInstagram(step))
+  saveCampaignSteps: (step) =>
+    dispatch(actionCreators.saveCampaignStepsInstagram(step)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Website);
--- components/Screens/InstagramCampaignCreate/styles/adObjectives.styles.js
+++ components/Screens/InstagramCampaignCreate/styles/adObjectives.styles.js
@@ -1,30 +1,30 @@
 import { StyleSheet, PixelRatio, I18nManager } from "react-native";
 import {
   widthPercentageToDP as wp,
-  heightPercentageToDP as hp
+  heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
 import { globalColors } from "../../../../GlobalStyles";
 export const colors = {
   black: "#1a1917",
   gray: "#888888",
   background1: "#751AFF",
-  background2: "#6268FF"
+  background2: "#6268FF",
 };
 const styles = StyleSheet.create({
   safeAreaView: {
     backgroundColor: "#0000",
-    height: "100%"
+    height: "100%",
   },
   container: {
     backgroundColor: "#0000",
     display: "flex",
-    justifyContent: "space-between"
+    justifyContent: "space-between",
     // zIndex: 5
   },
   backDrop: {
     position: "absolute",
     top: hp(5) < 30 ? -hp("73%") : -hp("63%"),
-    alignSelf: "center"
+    alignSelf: "center",
   },
   block1: {
     display: "flex",
@@ -32,21 +32,22 @@ const styles = StyleSheet.create({
     justifyContent: "flex-start",
     width: wp(100),
     paddingTop: 10,
-    zIndex: 1
+    zIndex: 1,
   },
   phoneicon: {
-    alignSelf: "center"
+    alignSelf: "center",
     // marginTop: 20
   },
   mainContent: {
     // flex: 1,
     // justifyContent: 'flex-start',
     paddingBottom: hp(14),
-    paddingTop: 20
+    paddingTop: 20,
+    paddingHorizontal: 26,
   },
   scrollViewStyle: {
     marginTop: 15,
-    zIndex: 2
+    zIndex: 2,
   },
   input1: {
     marginBottom: 30,
@@ -57,7 +58,7 @@ const styles = StyleSheet.create({
     borderColor: "transparent",
     // backgroundColor: '#751AFF',
     backgroundColor: "rgba(0,0,0,0.2)",
-    borderRadius: 30
+    borderRadius: 30,
   },
   inputLabel: {
     fontFamily: "montserrat-bold",
@@ -66,7 +67,7 @@ const styles = StyleSheet.create({
     alignSelf: "center",
     textAlign: "center",
     borderRadius: 30,
-    marginBottom: I18nManager.isRTL ? -16 : -10
+    marginBottom: I18nManager.isRTL ? -16 : -10,
   },
   inputText: {
     fontFamily: "montserrat-regular",
@@ -77,27 +78,27 @@ const styles = StyleSheet.create({
     width: "100%",
     paddingVertical: 10,
     borderBottomColor: "transparent",
-    height: 50
+    height: 50,
   },
   minBudget: {
     color: "#fff",
     alignSelf: "center",
     fontSize: 11,
-    fontFamily: "montserrat-regular"
+    fontFamily: "montserrat-regular",
   },
   title: {
     fontSize: 16,
     color: "#fff",
     textAlign: "center",
     fontFamily: "montserrat-bold",
-    paddingTop: 30
+    paddingTop: 30,
   },
   input2: {
     paddingHorizontal: 50,
     borderRadius: 30,
     alignSelf: "center",
     top: 20,
-    borderColor: "transparent"
+    borderColor: "transparent",
   },
   label: {
     fontFamily: "montserrat-regular",
@@ -105,19 +106,19 @@ const styles = StyleSheet.create({
     color: "#fff",
     alignSelf: "center",
     textAlign: "center",
-    width: "100%"
+    width: "100%",
   },
   downicon: {
     color: "#fff",
-    fontSize: 20
+    fontSize: 20,
   },
   popupOverlay: {
-    height: "100%"
+    height: "100%",
   },
   contentContainer: {
     marginTop: 15,
     paddingTop: 15,
-    marginBottom: 15
+    marginBottom: 15,
   },
   dateInput: {
     marginBottom: 5,
@@ -126,7 +127,7 @@ const styles = StyleSheet.create({
     alignSelf: "center",
     width: 300,
     height: 50,
-    justifyContent: "center"
+    justifyContent: "center",
     // borderWidth: 0.5,
   },
   dateLabel: {
@@ -134,38 +135,38 @@ const styles = StyleSheet.create({
     color: globalColors.orange,
     flexDirection: "column",
     fontFamily: "montserrat-regular",
-    fontSize: 16
+    fontSize: 16,
     // paddingHorizontal: 10
   },
   date: {
     fontFamily: "montserrat-bold",
     color: "#FF9D00",
     alignItems: "center",
-    fontSize: 14
+    fontSize: 14,
   },
   block1Title: {
-    top: hp(5)
+    top: hp(5),
   },
   innerBlock1: {
     flexDirection: "row",
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
   },
   dateContainer: {
     flexDirection: "row",
     justifyContent: "space-around",
-    alignItems: "center"
+    alignItems: "center",
   },
   dateColumn: {
     flexDirection: "column",
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
   },
   topContainer: {
     paddingVertical: 10,
     width: 300,
     flexDirection: "row",
-    alignSelf: "center"
+    alignSelf: "center",
     // shadowOffset: { width: 1, height: 1 },
     // shadowColor: 'black',
     // shadowOpacity: 0.2,
@@ -174,44 +175,44 @@ const styles = StyleSheet.create({
     borderTopStartRadius: 0,
     borderBottomStartRadius: 0,
     borderBottomEndRadius: 40,
-    borderTopEndRadius: 40
+    borderTopEndRadius: 40,
   },
   collectionAdLinkForm1: {
     borderBottomEndRadius: 0,
     borderTopEndRadius: 0,
     borderBottomStartRadius: 40,
-    borderTopStartRadius: 40
+    borderTopStartRadius: 40,
   },
   button: {
     backgroundColor: "rgba(0,0,0,0.2)",
     width: 150,
     height: 70,
     justifyContent: "center",
-    flexDirection: "column"
+    flexDirection: "column",
   },
   activeButton: {
     backgroundColor: "#FF9D00",
     height: 70,
     width: 150,
     justifyContent: "center",
-    flexDirection: "column"
+    flexDirection: "column",
   },
   inactiveText: {
     fontFamily: "montserrat-bold",
     fontSize: 11,
     color: "rgba(255,255,255,0.4)",
-    textAlign: "center"
+    textAlign: "center",
   },
   activeText: {
     fontFamily: "montserrat-bold",
     fontSize: 11,
     color: "#fff",
-    textAlign: "center"
+    textAlign: "center",
   },
   buttonSubText: {
     fontFamily: "montserrat-regular",
     paddingTop: 2,
-    fontSize: 12
+    fontSize: 12,
   },
   dateTextLabel: {
     borderTopLeftRadius: 150,
@@ -223,7 +224,7 @@ const styles = StyleSheet.create({
     alignSelf: "center",
     backgroundColor: "rgba(0,0,0,0.2)",
     height: 15,
-    zIndex: 1
+    zIndex: 1,
   },
   objectiveTextLabel: {
     marginTop: 30,
@@ -236,22 +237,22 @@ const styles = StyleSheet.create({
     alignSelf: "center",
     backgroundColor: "rgba(0,0,0,0.2)",
     height: 15,
-    zIndex: 1
+    zIndex: 1,
   },
   collectionAdView: {
-    paddingTop: 10
+    paddingTop: 10,
   },
   collectionAdText: {
     color: "#FFFFFF",
     fontSize: 12,
     fontFamily: "montserrat-bold",
-    textAlign: "center"
+    textAlign: "center",
   },
   proceedButtonRTL: {
     width: 65,
-    height: 65
+    height: 65,
     // padding: 20
-  }
+  },
 });
 
 export default styles;
