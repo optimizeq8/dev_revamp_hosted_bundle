5d4fc1e347e550c22a71b7f1af6dde2d2b3546f3
saadiyakazi <saadiya@optimizekw.com>
2020-06-07 17:16:19 +0300

    OTA 80 for analytics forget password screen

--- components/Screens/Menu/index.js
+++ components/Screens/Menu/index.js
@@ -442,7 +442,7 @@ class Menu extends Component {
               </TouchableOpacity>
               <Text style={styles.version}>
                 {translate("Version:")}
-                {Constants.nativeAppVersion}/79/
+                {Constants.nativeAppVersion}/80/
                 {Constants.nativeBuildVersion}
               </Text>
             </ScrollView>
