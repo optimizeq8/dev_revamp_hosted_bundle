614077959754efbc643be1b8fd79d09cd975a988
saadiyakazi <saadiya@optimizekw.com>
2020-06-07 20:19:16 +0300

    OTA 81 analytics for google keywords

--- components/Screens/Menu/index.js
+++ components/Screens/Menu/index.js
@@ -442,7 +442,7 @@ class Menu extends Component {
               </TouchableOpacity>
               <Text style={styles.version}>
                 {translate("Version:")}
-                {Constants.nativeAppVersion}/80/
+                {Constants.nativeAppVersion}/81/
                 {Constants.nativeBuildVersion}
               </Text>
             </ScrollView>
