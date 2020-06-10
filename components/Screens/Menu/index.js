c7860e511c2bb024fdedcc4cb04b585f29b425af
SamyOptimize <samy@optimizekw.com>
2020-06-10 11:47:32 +0300

    Update shell bundles for live with OTA 87

--- components/Screens/Menu/index.js
+++ components/Screens/Menu/index.js
@@ -442,7 +442,7 @@ class Menu extends Component {
               </TouchableOpacity>
               <Text style={styles.version}>
                 {translate("Version:")}
-                {Constants.nativeAppVersion}/86/
+                {Constants.nativeAppVersion}/87/
                 {Constants.nativeBuildVersion}
               </Text>
             </ScrollView>
