85a26b58f1b16003479e29673659bb8060492123
SamyOptimize <samy@optimizeapp.com>
2020-06-11 21:13:48 +0300

    Update bundles for live with OTA 91

--- components/Screens/Menu/index.js
+++ components/Screens/Menu/index.js
@@ -442,7 +442,7 @@ class Menu extends Component {
               </TouchableOpacity>
               <Text style={styles.version}>
                 {translate("Version:")}
-                {Constants.nativeAppVersion}/90/
+                {Constants.nativeAppVersion}/91/
                 {Constants.nativeBuildVersion}
               </Text>
             </ScrollView>
