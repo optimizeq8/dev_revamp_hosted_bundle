f610f4d9ba467cc322fb5528a9e557c43c6d5416
saadiyakazi <saadiya@optimizeapp.com>
2020-06-17 15:47:44 +0300

    [OA-187] Fix design for story ad cards and lomg press functionality to delete media

--- components/Screens/CampaignCreate/AdDesign/SnapAdCards/StoryAdCards.js
+++ components/Screens/CampaignCreate/AdDesign/SnapAdCards/StoryAdCards.js
@@ -7,49 +7,53 @@ import { heightPercentageToDP } from "react-native-responsive-screen";
 import AddCard from "./AddCard";
 import SnapCard from "./SnapCard";
 export default class StoryAdCards extends Component {
-  snapCards = item => {
-    if (item.index === this.props.StoryAdCards.length - 1) {
-      return (
-        <AddCard
-          arrayLen={this.props.StoryAdCards.length}
-          screenProps={this.props.screenProps}
-        />
-      );
-    } else
-      return (
-        <SnapCard
-          screenProps={this.props.screenProps}
-          rejected={this.props.rejected}
-          video={this.props.video}
-          // cancelUpload={this.props.cancelUpload}
-          _handleStoryAdCards={this.props._handleStoryAdCards}
-          removeSnapCard={this.props.removeSnapCard}
-          snapCardInfo={item}
-        />
-      );
+  snapCards = (item) => {
+    // if (item.index === this.props.StoryAdCards.length - 1) {
+    //   return (
+    //     <AddCard
+    //       arrayLen={this.props.StoryAdCards.length}
+    //       screenProps={this.props.screenProps}
+    //     />
+    //   );
+    // } else
+    return (
+      <SnapCard
+        screenProps={this.props.screenProps}
+        rejected={this.props.rejected}
+        video={this.props.video}
+        // cancelUpload={this.props.cancelUpload}
+        _handleStoryAdCards={this.props._handleStoryAdCards}
+        removeSnapCard={this.props.removeSnapCard}
+        snapCardInfo={item}
+      />
+    );
   };
   render() {
+    const { translate } = this.props.screenProps;
     return (
       <View
         style={{
           position: "absolute",
           top: "20%",
           width: "100%",
-          height: "80%"
+          height: "68%",
         }}
       >
         <FlatList
           contentContainerStyle={{
             paddingTop: 20,
             paddingBottom: 50,
-            alignItems: "center"
+            alignItems: "center",
           }}
-          keyExtractor={item => item.id}
+          keyExtractor={(item) => item.id}
           data={this.props.StoryAdCards}
           renderItem={this.snapCards}
           style={{}}
           numColumns={3}
         />
+        <Text style={styles.holdToDeleteText}>
+          {translate("Tap and hold to delete media")}
+        </Text>
       </View>
     );
   }
