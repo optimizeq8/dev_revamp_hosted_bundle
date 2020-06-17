92b629b20723a1c8784196b8c8dff1bc2c07e9c8
SamyOptimize <samy@optimizeapp.com>
2020-06-17 14:14:55 +0300

    [OA-186] Add TopStepsHeader componant

---
+++ components/MiniComponents/TopStepsHeader/index.js
@@ -0,0 +1,144 @@
+import React, { Component } from "react";
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  I18nManager,
+  StatusBar,
+} from "react-native";
+import { Badge } from "native-base";
+import { isUndefined } from "lodash";
+import analytics from "@segment/analytics-react-native";
+import * as Segment from "expo-analytics-segment";
+
+//icons
+import BackIcon from "../../../assets/SVGs/BackButtonPurple";
+import FowrwardIcon from "../../../assets/SVGs/ArrowForwardPurple";
+import InstagramIcon from "../../../assets/SVGs/InstagramIcon";
+import SnapchatIcon from "../../../assets/SVGs/Snapchat-Border";
+import GoogleSE from "../../../assets/SVGs/GoogleAds";
+
+//styles
+import styles from "./styles";
+
+export default class TopStepsHeader extends Component {
+  handleTouchableOpacity = () => {
+    let { segment, navigation, actionButton } = this.props;
+    if (!isUndefined(segment)) {
+      analytics.track(segment.source_action, {
+        source: segment.source,
+        source_action: segment.source_action,
+      });
+      Segment.trackWithProperties(segment.str, segment.obj);
+    }
+    if (!isUndefined(navigation)) navigation.goBack();
+    else actionButton();
+  };
+
+  handleProcessSteps = () => {
+    let { translate } = this.props.screenProps;
+    let steps = ["Details", "Compose", "Audience", "Payment"];
+    if (this.props.adType === "StoryAd") {
+      steps = ["Details", "Cover", "Compose", "Audience", "Payment"];
+    }
+    let stepsComponants = steps.map((step, i) => {
+      return (
+        <>
+          <View style={styles.badgeView}>
+            <Badge
+              style={
+                this.props.currentScreen === step
+                  ? styles.activeBadege
+                  : styles.badge
+              }
+            >
+              <Text
+                style={
+                  this.props.currentScreen === step
+                    ? styles.activeBadegeText
+                    : styles.badgeText
+                }
+              >
+                {translate(JSON.stringify(i + 1))}
+              </Text>
+            </Badge>
+            <Text
+              style={
+                this.props.currentScreen === step
+                  ? styles.activeTitleText
+                  : styles.badgeText
+              }
+            >
+              {translate(step)}
+            </Text>
+          </View>
+          {steps.length - 1 !== i && <View style={[styles.dash]} />}
+        </>
+      );
+    });
+    return stepsComponants;
+  };
+  render() {
+    let {
+      title,
+      actionButton,
+      containerStyle,
+      titelStyle,
+      icon,
+      translateTitle = true,
+      disabled = false,
+      iconColor = "#FFF",
+      adType,
+    } = this.props;
+    let { translate } = this.props.screenProps;
+    if (translateTitle)
+      if (title && typeof title === "object") {
+        title = title.map((text) => translate(text));
+        if (I18nManager.isRTL) {
+          title = title.reverse();
+        }
+      } else if (title && typeof title === "string") {
+        title = translate(title);
+      }
+    console.log(title);
+
+    return (
+      <View style={styles.progressCardView}>
+        <TouchableOpacity
+          disabled={disabled}
+          onPress={this.handleTouchableOpacity}
+          style={[
+            styles.left,
+            I18nManager.isRTL && {
+              transform: [{ rotateY: "180deg" }, { translateX: -13 }],
+            },
+          ]}
+        >
+          {I18nManager.isRTL ? <FowrwardIcon /> : <BackIcon />}
+        </TouchableOpacity>
+
+        <View style={styles.registerHeaderIconView}>
+          {icon === "snapchat" && (
+            <View style={{ paddingHorizontal: 5 }}>
+              <SnapchatIcon stroke="black" width={30} height={30} />
+            </View>
+          )}
+          {icon === "google" && (
+            <View style={{ paddingHorizontal: 5 }}>
+              <GoogleSE width={30} style={styles.googleIcon} />
+            </View>
+          )}
+          {icon === "instagram" && (
+            <View style={{ paddingHorizontal: 5 }}>
+              <InstagramIcon width={30} height={24} fill="#fff" />
+            </View>
+          )}
+          <Text style={styles.registerationText}>
+            {title && typeof title === "string" ? title : title.join(" ")}
+          </Text>
+        </View>
+        <View style={styles.content}>{this.handleProcessSteps()}</View>
+      </View>
+    );
+  }
+}
---
+++ components/MiniComponents/TopStepsHeader/styles.js
@@ -0,0 +1,166 @@
+import { StyleSheet, I18nManager } from "react-native";
+import {
+  heightPercentageToDP,
+  widthPercentageToDP,
+} from "react-native-responsive-screen";
+import { globalColors } from "../../../GlobalStyles";
+export const colors = {
+  black: "#1a1917",
+  gray: "#888888",
+  background1: "#751AFF",
+  background2: "#751AFF",
+};
+const styles = StyleSheet.create({
+  safeAreaViewContainer: {
+    flex: 1,
+    // backgroundColor: "#FFF"
+  },
+  subtitle: {
+    fontSize: 18,
+    color: "#fff",
+    textAlign: "center",
+    paddingTop: 13,
+    textAlign: "center",
+    fontFamily: "montserrat-medium",
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#0000",
+  },
+  content: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  mainCard: {
+    paddingTop: 0,
+    flex: 1,
+    marginLeft: 0,
+    marginRight: 0,
+  },
+  activeBadege: {
+    backgroundColor: globalColors.purple,
+    width: 35,
+    height: 35,
+    borderRadius: 35,
+    alignItems: "center",
+    justifyContent: "center",
+
+    alignSelf: "center",
+    paddingVertical: 0,
+  },
+  badge: {
+    alignSelf: "center",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "transparent",
+    width: 25,
+    height: 25,
+    borderRadius: 35,
+    borderColor: "#C6C6C6",
+    borderWidth: 1,
+    paddingVertical: 0,
+  },
+  activeBadegeText: {
+    marginTop: I18nManager.isRTL ? 10 : 0,
+    alignSelf: "center",
+    textAlignVertical: "center",
+    color: "#FFF",
+    fontSize: 14,
+    fontFamily: "montserrat-bold",
+  },
+  dash: {
+    alignSelf: "center",
+    width: 20,
+    marginBottom: 10,
+    marginHorizontal: -7,
+    borderBottomColor: "#C6C6C6",
+    borderBottomWidth: 2,
+  },
+  dashActive: {
+    marginLeft: -5,
+  },
+  badgeText: {
+    color: "#C6C6C6",
+    fontSize: 12,
+    fontFamily: "montserrat-light",
+  },
+  activeTitleText: {
+    fontFamily: "montserrat-bold",
+    fontSize: 12,
+    color: "#6C63FF",
+    textTransform: "uppercase",
+  },
+  titleText: {
+    fontFamily: "montserrat-regular",
+    fontSize: 8,
+    color: "#C6C6C6",
+    paddingTop: 3,
+  },
+  badgeView: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  progressCardView: {
+    display: "flex",
+    alignItems: "center",
+    backgroundColor: "#FFF",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    paddingVertical: 30,
+    borderBottomLeftRadius: 30,
+    borderBottomRightRadius: 30,
+  },
+  getStartedBtn: {
+    height: 54,
+    width: 220,
+    alignSelf: "center",
+  },
+  registerSuccessIcon: {
+    marginLeft: I18nManager.isRTL
+      ? widthPercentageToDP(-5)
+      : widthPercentageToDP(-55),
+    marginTop: heightPercentageToDP(5),
+  },
+  successText: {
+    fontFamily: "montserrat-regular",
+    fontSize: 14,
+    color: "#FFF",
+    textAlign: "left",
+  },
+  registerCompleteText: {
+    fontFamily: "montserrat-bold",
+    fontSize: 27,
+    color: "#FFF",
+    textAlign: "left",
+    paddingTop: 10,
+  },
+  mainView: {
+    display: "flex",
+    paddingHorizontal: 30,
+    paddingTop: 10,
+    flexDirection: "column",
+  },
+  getStartedText: {
+    fontSize: 14,
+  },
+  registerationText: {
+    fontSize: 14,
+    color: globalColors.purple,
+    textTransform: "uppercase",
+    fontFamily: "montserrat-bold",
+    letterSpacing: 0,
+    lineHeight: 18,
+    marginLeft: 5,
+  },
+  registerHeaderIconView: {
+    display: "flex",
+    alignItems: "center",
+    // flexDirection: "row",
+    // alignSelf: "flex-start"
+    // flex: 1
+  },
+});
+
+export default styles;
