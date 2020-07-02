5c5e6e9a548f2c0535258c881bf448b7b00ab1e7
SamyOptimize <samy@optimizeapp.com>
2020-07-02 17:55:57 +0300

    Add draggable markers and circles

--- components/MiniComponents/LocationMap/index.js
+++ components/MiniComponents/LocationMap/index.js
@@ -1,218 +1,77 @@
 import React, { Component } from "react";
 import { Text, View } from "react-native";
-import MapView, { PROVIDER_GOOGLE, Circle, Marker } from "react-native-maps";
+import MapView, {
+  PROVIDER_GOOGLE,
+  Circle,
+  Marker,
+  Callout,
+} from "react-native-maps";
+import { Button } from "native-base";
 
 export default class LocaionMap extends Component {
+  state = {
+    cirLat: 37.78825,
+    cirLong: -122.4324,
+    cirRad: 1000,
+    markers: [],
+    marker: { latitude: 37.78825, longitude: -122.4324 },
+  };
+  handleDrag = (e) => {
+    let cirLat = e.nativeEvent.coordinate.latitude;
+    let cirLong = e.nativeEvent.coordinate.longitude;
+    let marker = this.state.marker;
+    marker.coordinate = e.nativeEvent.coordinate;
+    this.setState({ cirLat, cirLong });
+  };
+  handleRad = (subtract = false) => {
+    let cirRad = subtract
+      ? this.state.marker.cirRad - 100
+      : this.state.marker.cirRad + 100;
+    let marker = this.state.marker;
+    marker.cirRad = cirRad;
+    this.setState({ cirRad });
+  };
+  handleAddCir = (e) => {
+    this.setState({
+      markers: [
+        ...this.state.markers,
+        {
+          coordinate: e.nativeEvent.coordinate,
+          key: JSON.stringify(e.nativeEvent.coordinate.latitude),
+          cirRad: 1000,
+        },
+      ],
+      marker: {
+        coordinate: e.nativeEvent.coordinate,
+        key: JSON.stringify(e.nativeEvent.coordinate.latitude),
+        cirRad: 1000,
+      },
+    });
+  };
+  handleMarkerSelect = (e, marker, onPress = false) => {
+    if (onPress) {
+      console.log(JSON.stringify(e.nativeEvent, null, 2));
+    }
+    this.setState({ marker });
+  };
+  handleMarkerLayput = (e, marker) => {
+    console.log(JSON.stringify(e.nativeEvent, null, 2));
+  };
   render() {
+    let { cirLat, cirLong, cirRad } = this.state;
     return (
-      <View style={{ height: 400, justifyContent: "center" }}>
+      <View
+        style={{
+          height: 400,
+          justifyContent: "center",
+          width: "100%",
+        }}
+      >
         <MapView
           provider={PROVIDER_GOOGLE}
           style={{ height: 400 }}
-          customMapStyle={[
-            {
-              elementType: "geometry",
-              stylers: [
-                {
-                  color: "#242f3e",
-                },
-              ],
-            },
-            {
-              elementType: "labels.text.fill",
-              stylers: [
-                {
-                  color: "#746855",
-                },
-              ],
-            },
-            {
-              elementType: "labels.text.stroke",
-              stylers: [
-                {
-                  color: "#242f3e",
-                },
-              ],
-            },
-            {
-              featureType: "administrative.locality",
-              elementType: "labels.text.fill",
-              stylers: [
-                {
-                  color: "#d59563",
-                },
-              ],
-            },
-            {
-              featureType: "poi",
-              elementType: "labels.text.fill",
-              stylers: [
-                {
-                  color: "#d59563",
-                },
-              ],
-            },
-            {
-              featureType: "poi.business",
-              stylers: [
-                {
-                  visibility: "off",
-                },
-              ],
-            },
-            {
-              featureType: "poi.park",
-              elementType: "geometry",
-              stylers: [
-                {
-                  color: "#263c3f",
-                },
-              ],
-            },
-            {
-              featureType: "poi.park",
-              elementType: "labels.text",
-              stylers: [
-                {
-                  visibility: "off",
-                },
-              ],
-            },
-            {
-              featureType: "poi.park",
-              elementType: "labels.text.fill",
-              stylers: [
-                {
-                  color: "#6b9a76",
-                },
-              ],
-            },
-            {
-              featureType: "road",
-              elementType: "geometry",
-              stylers: [
-                {
-                  color: "#38414e",
-                },
-              ],
-            },
-            {
-              featureType: "road",
-              elementType: "geometry.stroke",
-              stylers: [
-                {
-                  color: "#212a37",
-                },
-              ],
-            },
-            {
-              featureType: "road",
-              elementType: "labels.text.fill",
-              stylers: [
-                {
-                  color: "#9ca5b3",
-                },
-              ],
-            },
-            {
-              featureType: "road.arterial",
-              elementType: "labels",
-              stylers: [
-                {
-                  visibility: "off",
-                },
-              ],
-            },
-            {
-              featureType: "road.highway",
-              elementType: "geometry",
-              stylers: [
-                {
-                  color: "#746855",
-                },
-              ],
-            },
-            {
-              featureType: "road.highway",
-              elementType: "geometry.stroke",
-              stylers: [
-                {
-                  color: "#1f2835",
-                },
-              ],
-            },
-            {
-              featureType: "road.highway",
-              elementType: "labels",
-              stylers: [
-                {
-                  visibility: "off",
-                },
-              ],
-            },
-            {
-              featureType: "road.highway",
-              elementType: "labels.text.fill",
-              stylers: [
-                {
-                  color: "#f3d19c",
-                },
-              ],
-            },
-            {
-              featureType: "road.local",
-              stylers: [
-                {
-                  visibility: "off",
-                },
-              ],
-            },
-            {
-              featureType: "transit",
-              elementType: "geometry",
-              stylers: [
-                {
-                  color: "#2f3948",
-                },
-              ],
-            },
-            {
-              featureType: "transit.station",
-              elementType: "labels.text.fill",
-              stylers: [
-                {
-                  color: "#d59563",
-                },
-              ],
-            },
-            {
-              featureType: "water",
-              elementType: "geometry",
-              stylers: [
-                {
-                  color: "#17263c",
-                },
-              ],
-            },
-            {
-              featureType: "water",
-              elementType: "labels.text.fill",
-              stylers: [
-                {
-                  color: "#515c6d",
-                },
-              ],
-            },
-            {
-              featureType: "water",
-              elementType: "labels.text.stroke",
-              stylers: [
-                {
-                  color: "#17263c",
-                },
-              ],
-            },
-          ]}
+          onMarkerPress={(e) => console.log(e.nativeEvent)}
+          onLongPress={this.handleAddCir}
           initialRegion={{
             latitude: 37.78825,
             longitude: -122.4324,
@@ -220,30 +79,86 @@ export default class LocaionMap extends Component {
             longitudeDelta: 0.0421,
           }}
         >
-          <Marker
+          {this.state.markers.map((marker) => (
+            <>
+              <Marker
+                onLayout={this.handleMarkerLayput}
+                draggable={true}
+                geodesic={true}
+                onDragEnd={this.handleDrag}
+                coordinate={marker.coordinate}
+                onDragStart={(e) => this.handleMarkerSelect(e, marker)}
+                style={{ width: 80, height: 80 }}
+                title="Hiii"
+                // onDrag={this.handleDrag}
+                onSelect={(e) => this.handleMarkerSelect(e, marker, true)}
+              />
+              {/* <Callout style={{ width: 200 }}>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    width: "100%",
+                    justifyContent: "space-evenly",
+                  }}
+                >
+                  <Button
+                    style={{ width: 50, justifyContent: "center" }}
+                    onPress={() => this.handleRad(false)}
+                  >
+                    <Text>+</Text>
+                  </Button>
+                  <Button
+                    style={{ width: 50, justifyContent: "center" }}
+                    onPress={() => this.handleRad(true)}
+                  >
+                    <Text>-</Text>
+                  </Button>
+                </View>
+              </Callout>
+              */}
+              <Circle
+                onLayout={() => {
+                  if (this.cir) {
+                    this.cir.setNativeProps({
+                      fillColor: "#7778",
+                    });
+                  }
+                }}
+                ref={(ref) => {
+                  this.cir = ref;
+                }}
+                center={marker.coordinate}
+                // center={{ latitude: cirLat, longitude: cirLong }}
+                radius={marker.cirRad}
+                fillColor={"rgba(132,22,25,1)"}
+              />
+            </>
+          ))}
+          {/* <Marker
             draggable={true}
             geodesic={true}
-            coordinate={{ latitude: 37.78825, longitude: -122.4324 }}
+            coordinate={{
+              latitude: 37.78825,
+              longitude: -122.4324,
+            }}
             style={{ width: 80, height: 80 }}
-            anchor={{ x: 0.5, y: 0.5 }}
-          >
-            <View
-              style={{
-                height: "100%",
-                width: "100%",
-                borderRadius: 50,
-                backgroundColor: "#3f35",
-                zIndex: 10,
-                position: "absolute",
-                alignSelf: "center",
-              }}
-            />
-          </Marker>
-          <Circle
-            center={{ latitude: 37.78825, longitude: -122.4324 }}
-            radius={1000}
-            fillColor={"rgba(132,22,25,1"}
+            onDrag={this.handleDrag}
           />
+          <Circle
+            onLayout={() => {
+              if (this.cir) {
+                this.cir.setNativeProps({
+                  fillColor: "#7778",
+                });
+              }
+            }}
+            ref={(ref) => {
+              this.cir = ref;
+            }}
+            center={{ latitude: cirLat, longitude: cirLong }}
+            radius={cirRad}
+            fillColor={"rgba(132,22,25,1)"}
+          /> */}
         </MapView>
       </View>
     );
