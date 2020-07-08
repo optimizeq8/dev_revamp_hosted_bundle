7b7b3ba033ea54dc3b3e3be4310477ba89138c56
SamyOptimize <samy@optimizeapp.com>
2020-07-08 15:24:09 +0300

    Add build files for sprint version 0.3950

--- components/Screens/Menu/index.js
+++ components/Screens/Menu/index.js
@@ -442,7 +442,7 @@ class Menu extends Component {
               </TouchableOpacity>
               <Text style={styles.version}>
                 {translate("Version:")}
-                {Constants.nativeAppVersion}/118/
+                {Constants.nativeAppVersion}/119/
                 {Constants.nativeBuildVersion}
               </Text>
             </ScrollView>
