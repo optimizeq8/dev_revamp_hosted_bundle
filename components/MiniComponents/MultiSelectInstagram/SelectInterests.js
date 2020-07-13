3d901db0aa9346bf49dec7a7bb25b1ab1545273f
SamyOptimize <samy@optimizeapp.com>
2020-07-13 21:29:17 +0300

    OA-334 add custom interest search

--- components/MiniComponents/MultiSelectInstagram/SelectInterests.js
+++ components/MiniComponents/MultiSelectInstagram/SelectInterests.js
@@ -22,7 +22,7 @@ import LowerButton from "../LowerButton";
 import GradientButton from "../GradientButton";
 
 class SelectInterests extends Component {
-  state = { interests: null, open: false };
+  state = { interests: null, customInterests: null, open: false };
   componentDidMount() {
     Segment.screen("Interests Options");
 
@@ -54,9 +54,33 @@ class SelectInterests extends Component {
         this.setState({ interests });
       }
     }
+    if (
+      this.props.customInterests &&
+      prevProps.customInterests !== this.props.customInterests
+    ) {
+      let customInterests = [];
+      if (this.props.customInterests.length === 0) {
+        this.setState({ customInterests: [] });
+      } else {
+        customInterests =
+          this.props.customInterests &&
+          Object.keys(this.props.customInterests).map((interest) => {
+            return {
+              id: interest,
+              name: interest,
+              subcat: [...this.props.customInterests[interest]],
+            };
+          });
+
+        this.setState({ customInterests });
+      }
+    }
   }
   handleSideMenu = () => {
     this.props._handleSideMenuState(false);
+  };
+  handleCustomInterests = (event) => {
+    this.props.get_custom_interests_instagram(event.nativeEvent.text);
   };
   render() {
     const { translate } = this.props.screenProps;
@@ -74,18 +98,35 @@ class SelectInterests extends Component {
             </Text>
 
             <View style={styles.slidercontainer}>
-              <GradientButton
-                style={[
-                  styles.toggleSelectorButton,
-                  {
-                    opacity: this.props.country_code === "" ? 0.5 : 1,
-                  },
-                ]}
-                onPressAction={() => this.setState({ open: true })}
-              >
-                <PlusCircle width={53} height={53} />
-              </GradientButton>
-              <Picker
+              <View style={{ flexDirection: "row", alignSelf: "center" }}>
+                <GradientButton
+                  style={[
+                    styles.toggleSelectorButton,
+                    {
+                      opacity: this.props.country_code === "" ? 0.5 : 1,
+                    },
+                  ]}
+                  onPressAction={() => this.setState({ open: true })}
+                >
+                  <PlusCircle width={53} height={53} />
+                </GradientButton>
+                <GradientButton
+                  style={[
+                    styles.toggleSelectorButton,
+                    {
+                      opacity: this.props.country_code === "" ? 0.5 : 1,
+                    },
+                  ]}
+                  onPressAction={() => this.setState({ open2: true })}
+                >
+                  <Icon
+                    style={{ color: "#fff" }}
+                    name="edit"
+                    type="MaterialIcons"
+                  />
+                </GradientButton>
+              </View>
+              {/* <Picker
                 showDropDowns={true}
                 readOnlyHeadings={true}
                 screenProps={this.props.screenProps}
@@ -105,6 +146,28 @@ class SelectInterests extends Component {
                 single={false}
                 screenName={"Select Interests"}
                 closeCategoryModal={() => this.setState({ open: false })}
+              /> */}
+              <Picker
+                showDropDowns={true}
+                readOnlyHeadings={true}
+                screenProps={this.props.screenProps}
+                searchPlaceholderText={translate("Search for custom Interests")}
+                data={this.state.customInterests}
+                uniqueKey={"id"}
+                displayKey={"name"}
+                subKey="subcat"
+                selectChildren={true}
+                open={this.state.open2}
+                onSelectedItemsChange={this.props.onSelectedItemsChange}
+                onSelectedItemObjectsChange={
+                  this.props.onSelectedItemObjectsChange
+                }
+                showIcon={false}
+                selectedItems={this.props.selectedItems}
+                single={false}
+                screenName={"Select Interests"}
+                closeCategoryModal={() => this.setState({ open2: false })}
+                customSearch={this.handleCustomInterests}
               />
               {isNull(this.state.interests) && (
                 <ActivityIndicator color="#FFFF" size="large" />
@@ -126,10 +189,13 @@ const mapStateToProps = (state) => ({
   campaign_id: state.instagramAds.campaign_id,
   mainBusiness: state.account.mainBusiness,
   interests: state.instagramAds.interests,
+  customInterests: state.instagramAds.customInterests,
 });
 
 const mapDispatchToProps = (dispatch) => ({
   get_interests_instagram: () =>
     dispatch(actionCreators.get_interests_instagram()),
+  get_custom_interests_instagram: (keyword) =>
+    dispatch(actionCreators.get_custom_interests_instagram(keyword)),
 });
 export default connect(mapStateToProps, mapDispatchToProps)(SelectInterests);
