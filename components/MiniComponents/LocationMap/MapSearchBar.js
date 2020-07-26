934b0305915344b26a29f34a3286f297daeb69d9
SamyOptimize <samy@optimizeapp.com>
2020-07-26 20:30:51 +0300

    OA-401 add google auto complete

--- components/MiniComponents/LocationMap/MapSearchBar.js
+++ components/MiniComponents/LocationMap/MapSearchBar.js
@@ -5,6 +5,7 @@ import { SafeAreaView } from "react-navigation";
 import Axios from "axios";
 import countriesBillingAddress from "../../Data/countries.billingAddress";
 import SearchResault from "./SearchResault";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 
 export default class MapSearchBar extends Component {
   state = { searchValue: "", autoFeatures: "" };
@@ -23,7 +24,7 @@ export default class MapSearchBar extends Component {
         (country) => country.value === this.props.country_code
       ).label;
     Axios.get(
-      `https://api.geocodify.com/v2/geocode?api_key=710870f62a633c72ad91bc4ab46c4aee88e4dd78&q=${location}`
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=AIzaSyCPCME2BWXM3bRzNdvrGHAvnOxB3np3c_Q`
     )
       .then((res) => res.data)
       .then((data) => {
@@ -41,21 +42,49 @@ export default class MapSearchBar extends Component {
     this.SearchFilterFunction(location);
     setTimeout(() => {
       Axios.get(
-        `https://api.geocodify.com/v2/autocomplete?api_key=710870f62a633c72ad91bc4ab46c4aee88e4dd78&q=${location}`
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=AIzaSyCPCME2BWXM3bRzNdvrGHAvnOxB3np3c_Q`
       )
         .then((res) => res.data)
         .then((data) => {
-          let autoFeatures = data.response.features;
-          this.props.handleAutoFeatures(autoFeatures);
+          console.log(JSON.stringify(data, null, 2));
+
+          let results = data.results;
+          this.props.handleAutoFeatures(results);
           // this.props.handleShowFlatList(true);
         })
         .catch((err) => console.log(err));
     }, 1000);
   };
+  handleCountrySelection = (data, details) => {
+    let marker = {
+      latitude: details.geometry.location.lat,
+      longitude: details.geometry.location.lng,
+      radius: 5000,
+    };
+    this.props.handleMarkers(marker);
+    this.props.handleLocationSearchModal(false);
+  };
   render() {
     return (
-      <View style={{ width: "100%" }}>
-        <SearchBar
+      <View style={{ width: "100%", height: "100%" }}>
+        <GooglePlacesAutocomplete
+          placeholder="Search"
+          fetchDetails={true}
+          onPress={(data, details = null) => {
+            // 'details' is provided when fetchDetails = true
+            this.handleCountrySelection(data, details);
+            // console.log(
+            //   JSON.stringify(data, null, 2),
+            //   JSON.stringify(details, null, 2)
+            // );
+          }}
+          nearbyPlacesAPI={"GooglePlacesSearch"}
+          query={{
+            key: "AIzaSyCPCME2BWXM3bRzNdvrGHAvnOxB3np3c_Q",
+            language: "en",
+          }}
+        />
+        {/* <SearchBar
           round
           containerStyle={{
             backgroundColor: "#fff",
@@ -78,7 +107,7 @@ export default class MapSearchBar extends Component {
           onClear={(text) => this.SearchFilterFunction("")}
           placeholder="Search for country or region"
           value={this.state.searchValue}
-        />
+        /> */}
       </View>
     );
   }
