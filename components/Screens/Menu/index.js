312c2ec5e452cf2bbe97ac393cff9894bc8a5005
SamyOptimize <samy@optimizeapp.com>
2020-08-05 17:15:04 +0300

    OTA 145 for live

--- components/Screens/Menu/index.js
+++ components/Screens/Menu/index.js
@@ -442,7 +442,7 @@ class Menu extends Component {
               </TouchableOpacity>
               <Text style={styles.version}>
                 {translate("Version:")}
-                {Constants.nativeAppVersion}/144/
+                {Constants.nativeAppVersion}/145/
                 {Constants.nativeBuildVersion}
               </Text>
             </ScrollView>
