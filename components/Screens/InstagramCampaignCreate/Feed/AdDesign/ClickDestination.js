9a7a278bca3b9eed6ab6e5f8a8ce8fe65bc6c696
saadiyakazi <saadiya@optimizeapp.com>
2020-08-18 13:52:04 +0300

    Change click destination for instagram feed ad

--- components/Screens/InstagramCampaignCreate/Feed/AdDesign/ClickDestination.js
+++ components/Screens/InstagramCampaignCreate/Feed/AdDesign/ClickDestination.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import { Text, View, LayoutAnimation } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
+import { widthPercentageToDP } from "react-native-responsive-screen";
 import styles from "../../styles/adDesign.styles";
 import { globalColors } from "../../../../../GlobalStyles";
 import ArrowUp from "../../../../../assets/SVGs/ArrowUp";
 import InstagramSwipeUpDestination from "../../SwipeUpDestination/index";
 import { Icon } from "native-base";
 import { Platform } from "react-native";
+import { instagramAdObjectives } from "../../../../Data/instagramObjectives.data";
 export default class ClickDestination extends Component {
   state = {
     swipeUpMinHeight: 0,
@@ -49,6 +51,11 @@ export default class ClickDestination extends Component {
             (this.props.existingPosts ? this.state.compHeight : 0),
           zIndex: 10,
         };
+
+    let SwipeIcon = instagramAdObjectives["InstagramFeedAd"].find(
+      (obj) =>
+        obj.value === (this.props.campaignInfo.objective || "BRAND_AWARENESS")
+    ).icon;
     return (
       <View
         style={{
@@ -70,36 +77,65 @@ export default class ClickDestination extends Component {
           disabled={this.props.loading || this.state.expanded}
         >
           {!this.state.expanded ? (
-            <>
-              <ArrowUp stroke={globalColors.orange} />
-              <Text style={styles.destinationText}>
-                {this.props.data &&
-                this.props.data.link &&
-                this.props.data.link !== "BLANK" &&
-                ["link", "BLANK", "app_install"].includes(
-                  this.props.campaignInfo.destination
-                )
-                  ? this.props.campaignInfo.destination === "link" ||
-                    (this.props.data.objective === "BRAND_AWARENESS" &&
-                      this.props.campaignInfo.destination === "BLANK")
-                    ? translate("Website")
-                    : this.props.campaignInfo.destination === "app_install"
-                    ? translate("App Installs")
-                    : this.props.data.objective === "VIDEO_VIEWS"
-                    ? translate("Video Views")
-                    : translate("Click destination")
-                  : translate("Click destination")}
-              </Text>
-              {this.props.data &&
-              this.props.data.link &&
-              this.props.data.link !== "BLANK" &&
-              (this.props.campaignInfo.destination === "link" ||
-                this.props.campaignInfo.destination === "BLANK") ? (
-                <Text numberOfLines={1} style={styles.websiteLink}>
-                  {this.props.data.link}
-                </Text>
-              ) : null}
-            </>
+            <View style={{ paddingLeft: 10 }}>
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                  // backgroundColor: "red",
+                  // width: "90%",
+                  alignItems: "center",
+                }}
+              >
+                <SwipeIcon
+                  width={widthPercentageToDP(10)}
+                  height={widthPercentageToDP(10)}
+                  fill="#000"
+                  stroke="#000"
+                />
+                <View style={styles.swipeUpView}>
+                  <Text style={[styles.swipeUpText, { fontSize: 9 }]}>
+                    {translate("Click settings")}
+                  </Text>
+                  <Text style={styles.swipeUpText}>
+                    {this.props.data &&
+                    this.props.data.link &&
+                    this.props.data.link !== "BLANK" &&
+                    ["link", "BLANK", "app_install"].includes(
+                      this.props.campaignInfo.destination
+                    )
+                      ? this.props.campaignInfo.destination === "link" ||
+                        (this.props.data.objective === "BRAND_AWARENESS" &&
+                          this.props.campaignInfo.destination === "BLANK")
+                        ? translate("Website")
+                        : this.props.campaignInfo.destination === "app_install"
+                        ? translate("App Installs")
+                        : this.props.data.objective === "VIDEO_VIEWS"
+                        ? translate("Video Views")
+                        : translate("Click destination")
+                      : translate("Click destination")}
+                  </Text>
+                  {this.props.data &&
+                  this.props.data.link &&
+                  this.props.data.link !== "BLANK" &&
+                  (this.props.campaignInfo.destination === "link" ||
+                    this.props.campaignInfo.destination === "BLANK") ? (
+                    <Text
+                      numberOfLines={1}
+                      style={[[styles.swipeUpText, styles.swipeUpSubText]]}
+                    >
+                      {this.props.data.link}
+                    </Text>
+                  ) : null}
+                </View>
+              </View>
+              <Icon
+                type="MaterialIcons"
+                name="keyboard-arrow-down"
+                fontSize={50}
+                style={styles.downIcon}
+              />
+            </View>
           ) : (
             <>
               <TouchableOpacity
