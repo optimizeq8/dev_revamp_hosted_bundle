5f09e7a8817493a60917948d348f96de47bbd67c
SamyOptimize <samy@optimizeapp.com>
2020-08-19 16:43:44 +0300

    OTA 187 for sprint

--- components/Screens/Menu/index.js
+++ components/Screens/Menu/index.js
@@ -443,7 +443,7 @@ class Menu extends Component {
               </TouchableOpacity>
               <Text style={styles.version}>
                 {translate("Version:")}
-                {Constants.nativeAppVersion}/186/
+                {Constants.nativeAppVersion}/187/
                 {Constants.nativeBuildVersion}
               </Text>
             </ScrollView>
