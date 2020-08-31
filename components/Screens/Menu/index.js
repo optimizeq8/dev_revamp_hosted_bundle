63799ea7a77eca08d7738dcc02e577e1c69c7231
saadiyakazi <saadiya@optimizeapp.com>
2020-08-31 21:54:15 +0300

    OTA LIVE 197 with 2 params for registeration

--- components/Screens/Menu/index.js
+++ components/Screens/Menu/index.js
@@ -443,7 +443,7 @@ class Menu extends Component {
               </TouchableOpacity>
               <Text style={styles.version}>
                 {translate("Version:")}
-                {Constants.nativeAppVersion}/196/
+                {Constants.nativeAppVersion}/197/
                 {Constants.nativeBuildVersion}
               </Text>
             </ScrollView>
