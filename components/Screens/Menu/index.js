e495d1f2c8786594492ff149ecf3cb960ce7c45c
saadiyakazi <saadiya@optimizeapp.com>
2020-09-15 11:29:23 +0300

    OTA 204 fix loader disable

--- components/Screens/Menu/index.js
+++ components/Screens/Menu/index.js
@@ -443,7 +443,7 @@ class Menu extends Component {
               </TouchableOpacity>
               <Text style={styles.version}>
                 {translate("Version:")}
-                {Constants.nativeAppVersion}/203/
+                {Constants.nativeAppVersion}/204/
                 {Constants.nativeBuildVersion}
               </Text>
             </ScrollView>
