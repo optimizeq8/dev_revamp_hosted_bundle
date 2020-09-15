87e6f887988e07fd8733a89086ef5fa046e0733a
saadiyakazi <saadiya@optimizeapp.com>
2020-09-15 14:10:02 +0300

    OTA 206 dev-01

--- components/Screens/Menu/index.js
+++ components/Screens/Menu/index.js
@@ -443,7 +443,7 @@ class Menu extends Component {
               </TouchableOpacity>
               <Text style={styles.version}>
                 {translate("Version:")}
-                {Constants.nativeAppVersion}/205/
+                {Constants.nativeAppVersion}/206/
                 {Constants.nativeBuildVersion}
               </Text>
             </ScrollView>
