fd9394685ae81d9f0ac7ff4adeaba006a8774ca7
saadiyakazi <saadiya@optimizeapp.com>
2020-09-15 13:25:01 +0300

    OTA 205 for fixing min age slider

--- components/Screens/Menu/index.js
+++ components/Screens/Menu/index.js
@@ -443,7 +443,7 @@ class Menu extends Component {
               </TouchableOpacity>
               <Text style={styles.version}>
                 {translate("Version:")}
-                {Constants.nativeAppVersion}/204/
+                {Constants.nativeAppVersion}/205/
                 {Constants.nativeBuildVersion}
               </Text>
             </ScrollView>
