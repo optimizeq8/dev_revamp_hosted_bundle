f71f2f31e9d4677d58bfe20fed2642286b763d8b
saadiyakazi <saadiya@optimizeapp.com>
2020-09-15 13:20:45 +0300

    remove segments from ageoption menu

--- components/MiniComponents/AgeOptions/AgeOption.js
+++ components/MiniComponents/AgeOptions/AgeOption.js
@@ -4,7 +4,6 @@ import { Text, Container } from "native-base";
 import { SafeAreaView } from "react-navigation";
 import MultiSlider from "@ptomasroos/react-native-multi-slider";
 import RangeMarkers from "./RangeMarkers";
-import * as Segment from "expo-analytics-segment";
 
 //Styles
 import styles from "./styles";
@@ -23,7 +22,6 @@ export default class AgeOption extends Component {
   };
 
   componentDidMount() {
-    Segment.screen("Age Option");
     this.setState({
       values: [this.props.minAge, this.props.maxAge],
     });
