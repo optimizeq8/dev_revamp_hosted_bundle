87e064b00b1c9a085db923469f94a0ce2a1effd0
saadiyakazi <saadiya@optimizeapp.com>
2020-09-16 10:19:21 +0300

    OTA 208 app choice fixes

--- components/Screens/Menu/index.js
+++ components/Screens/Menu/index.js
@@ -443,7 +443,7 @@ class Menu extends Component {
               </TouchableOpacity>
               <Text style={styles.version}>
                 {translate("Version:")}
-                {Constants.nativeAppVersion}/207/
+                {Constants.nativeAppVersion}/208/
                 {Constants.nativeBuildVersion}
               </Text>
             </ScrollView>
