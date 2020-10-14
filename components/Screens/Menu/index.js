742f39b939fb7f4ea006c1a6c4bbf94fd59244bf
saadiyakazi <saadiya@optimizeapp.com>
2020-10-14 15:41:59 +0300

    OTA 249 design changes for rejected reason modal

--- components/Screens/Menu/index.js
+++ components/Screens/Menu/index.js
@@ -409,7 +409,7 @@ class Menu extends Component {
               </TouchableOpacity>
               <Text style={styles.version}>
                 {translate("Version:")}
-                {Constants.nativeAppVersion}/248/
+                {Constants.nativeAppVersion}/249/
                 {Constants.nativeBuildVersion}
               </Text>
             </ScrollView>
