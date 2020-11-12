2c6b580a453c6af2eb6d1c2192e3a5876654f9f4
SamyOptimize <samy@optimizeapp.com>
2020-11-12 14:32:35 +0300

    Add rn-placeholdermodify campagin card loading and campagin details loading

--- components/MiniComponents/PlaceholderLine.js
+++ components/MiniComponents/PlaceholderLine.js
@@ -1,26 +1,32 @@
 import React, { Component } from "react";
 import { Text, View } from "react-native";
 import * as Animatable from "react-native-animatable";
+import {
+  Placeholder,
+  PlaceholderMedia,
+  PlaceholderLine,
+  Shine,
+} from "rn-placeholder";
 
-export default class PlaceholderLine extends Component {
+export default class PlaceholderLineComp extends Component {
   render() {
     let { width, height, color, marginVertical } = this.props;
 
     return (
-      <Animatable.View
-        animation="flash"
-        iterationCount="infinite"
-        duration={2500}
-        easing="linear"
-        style={{
-          width: width ? width : 100,
-          height: height ? height : 20,
-          backgroundColor: color ? color : "rgba(255,157,0,0.8)",
-          borderRadius: 20,
-          alignSelf: "center",
-          marginVertical: marginVertical ? marginVertical : 5
-        }}
-      />
+      <Placeholder Animation={Shine}>
+        <PlaceholderLine
+          style={[
+            {
+              width: width ? width : 100,
+              height: height ? height : 20,
+              backgroundColor: color ? color : "#0002",
+              borderRadius: 20,
+              alignSelf: "center",
+              marginVertical: marginVertical ? marginVertical : 5,
+            },
+          ]}
+        />
+      </Placeholder>
     );
   }
 }
--- components/Screens/CampaignDetails/styles.js
+++ components/Screens/CampaignDetails/styles.js
@@ -153,7 +153,6 @@ const styles = StyleSheet.create({
   backgroundViewWrapper: {
     justifyContent: "center",
     overflow: "hidden",
-    backgroundColor: "#0004",
     width: "100%",
     height: "100%",
     borderRadius: 40,
