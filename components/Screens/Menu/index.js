8ade36f5ba832b3536d822eaebd5a0beaffffac0
saadiyakazi <saadiya@optimizeapp.com>
2020-11-12 13:40:08 +0300

    OTA LIVE 288 app install fix

--- components/Screens/Menu/index.js
+++ components/Screens/Menu/index.js
@@ -409,7 +409,7 @@ class Menu extends Component {
               </TouchableOpacity>
               <Text style={styles.version}>
                 {translate("Version:")}
-                {Constants.nativeAppVersion}/287/
+                {Constants.nativeAppVersion}/288/
                 {Constants.nativeBuildVersion}
               </Text>
             </ScrollView>
