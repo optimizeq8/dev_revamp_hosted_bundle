0fbbb14b24e5437aa65d81ace75e2becec693f68
saadiyakazi <saadiya@optimizeapp.com>
2020-11-19 13:26:49 +0300

    Fix changes for the instagram ad target story|

--- components/Screens/InstagramCampaignCreate/Story/AdTargetting/BudgetCards/index.js
+++ components/Screens/InstagramCampaignCreate/Story/AdTargetting/BudgetCards/index.js
@@ -71,76 +71,31 @@ export class BudgetCards extends Component {
     ));
 
     return (
-      <View style={{ height: "10%" }}>
-        <MaskedView
-          maskElement={
-            <LinearGradient
-              colors={["black", "transparent"]}
-              start={[0.85, 0]}
-              end={[this.state.scrollX, 0]}
-              style={{ height: "100%", width: "95%" }}
-            />
-          }
+      <View>
+        <ScrollView
+          onScroll={this.handleFading}
+          scrollEventThrottle={100}
+          horizontal
+          style={styles.budgetCardsStyle}
+          contentContainerStyle={styles.scrollContainerStyle}
+          showsHorizontalScrollIndicator={false}
         >
-          <ScrollView
-            onScroll={this.handleFading}
-            scrollEventThrottle={100}
-            horizontal
-            style={styles.budgetCardsStyle}
-            contentContainerStyle={styles.scrollContainerStyle}
-            showsHorizontalScrollIndicator={false}
+          <View
+            style={[
+              styles.budgetCardStyle,
+              budgetOption === 0 ? { borderWidth: 2.5 } : { borderWidth: 0 },
+            ]}
           >
-            <View
-              style={[
-                styles.budgetCardStyle,
-                budgetOption === 0 ? { borderWidth: 2.5 } : { borderWidth: 0 },
-              ]}
-            >
-              {budgetOption !== 0 ||
-              (value === "$0" && !this.state.placeholder) ? (
-                <TouchableOpacity
-                  style={{ flex: 1, justifyContent: "center" }}
-                  onPress={() => {
-                    this.setState({ placeholder: true });
-                    _handleBudget("$0", 0, false, 0);
-                  }}
-                >
-                  <Text
-                    style={[
-                      styles.budget,
-                      {
-                        fontSize:
-                          budgetOption !== 0 ||
-                          (value === "$0" && !this.state.placeholder)
-                            ? 10
-                            : 20,
-                      },
-                    ]}
-                  >
-                    {translate("Custom Budget")}
-                  </Text>
-                </TouchableOpacity>
-              ) : (
-                <TextInputMask
-                  includeRawValueInChangeText
-                  type={"money"}
-                  options={{
-                    precision: 0,
-                    delimiter: ",",
-                    unit: "$",
-                  }}
-                  focus={this.state.placeholder}
-                  maxLength={8}
-                  value={this.state.customValue}
-                  onChangeText={this.handleCustomBudgetChange}
-                  onFocus={() => {
-                    this.setState({ placeholder: true });
-                    _handleBudget("$0", 0, false, 0);
-                  }}
-                  onBlur={() => {
-                    _handleBudget(value, lifetime_budget_micro, true, 0);
-                    this.setState({ placeholder: false });
-                  }}
+            {budgetOption !== 0 ||
+            (value === "$0" && !this.state.placeholder) ? (
+              <TouchableOpacity
+                style={{ flex: 1, justifyContent: "center" }}
+                onPress={() => {
+                  this.setState({ placeholder: true });
+                  _handleBudget("$0", 0, false, 0);
+                }}
+              >
+                <Text
                   style={[
                     styles.budget,
                     {
@@ -151,13 +106,47 @@ export class BudgetCards extends Component {
                           : 20,
                     },
                   ]}
-                  ref={(ref) => (this.moneyField = ref)}
-                />
-              )}
-            </View>
-            {cards}
-          </ScrollView>
-        </MaskedView>
+                >
+                  {translate("Custom Budget")}
+                </Text>
+              </TouchableOpacity>
+            ) : (
+              <TextInputMask
+                includeRawValueInChangeText
+                type={"money"}
+                options={{
+                  precision: 0,
+                  delimiter: ",",
+                  unit: "$",
+                }}
+                focus={this.state.placeholder}
+                maxLength={8}
+                value={this.state.customValue}
+                onChangeText={this.handleCustomBudgetChange}
+                onFocus={() => {
+                  this.setState({ placeholder: true });
+                  _handleBudget("$0", 0, false, 0);
+                }}
+                onBlur={() => {
+                  _handleBudget(value, lifetime_budget_micro, true, 0);
+                  this.setState({ placeholder: false });
+                }}
+                style={[
+                  styles.budget,
+                  {
+                    fontSize:
+                      budgetOption !== 0 ||
+                      (value === "$0" && !this.state.placeholder)
+                        ? 10
+                        : 20,
+                  },
+                ]}
+                ref={(ref) => (this.moneyField = ref)}
+              />
+            )}
+          </View>
+          {cards}
+        </ScrollView>
       </View>
     );
   }
