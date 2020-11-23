ab53bd1e84a86ab7258dd1bd241511ab13fa92f9
SamyOptimize <samy@optimizeapp.com>
2020-11-23 16:30:22 +0300

    Modify the loaded modules at app start up

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,10 @@
 const { getDefaultConfig } = require("metro-config");
+const modulePaths = require("./packager/modulePaths");
+const resolve = require("path").resolve;
+const fs = require("fs");
+const ROOT_FOLDER = resolve(__dirname, "./");
 
-module.exports = (async () => {
+const config = (async () => {
   const {
     resolver: { sourceExts, assetExts },
   } = await getDefaultConfig();
@@ -8,10 +12,25 @@ module.exports = (async () => {
     transformer: {
       babelTransformerPath: require.resolve("react-native-svg-transformer"),
       assetPlugins: ["expo-asset/tools/hashAssetFiles"],
+      getTransformOptions: () => {
+        const moduleMap = {};
+        modulePaths.forEach((path) => {
+          if (fs.existsSync(path)) {
+            moduleMap[resolve(path)] = true;
+          }
+        });
+        return {
+          preloadedModules: moduleMap,
+          transform: { inlineRequires: { blacklist: moduleMap } },
+        };
+      },
     },
     resolver: {
       assetExts: assetExts.filter((ext) => ext !== "svg"),
       sourceExts: [...sourceExts, "svg"],
     },
+    projectRoot: ROOT_FOLDER,
   };
 })();
+
+module.exports = config;
