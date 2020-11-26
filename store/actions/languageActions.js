983dd8c117165a482895d485552d88120ab32afd
saadiyakazi <saadiya@optimizeapp.com>
2020-11-26 10:16:11 +0300

    CCHanges still need to be made for language

--- store/actions/languageActions.js
+++ store/actions/languageActions.js
@@ -27,73 +27,105 @@ export const getLanguageListPOEdit = (language) => {
           language,
         },
       });
-      const response = await axios.post(
-        "https://api.poeditor.com/v2/terms/list",
-        qs.stringify({
-          api_token: "12aec028da2333797aaaa1768d444fb9",
-          id: "283545",
-          language,
-        })
-      );
+      axios
+        .post(
+          "https://api.poeditor.com/v2/terms/list",
+          qs.stringify({
+            api_token: "12aec028da2333797aaaa1768d444fb9",
+            id: "283545",
+            language,
+          })
+        )
+        .then((response) => response.data)
+        .then((data) => {
+          if (data.response.status === "success") {
+            AsyncStorage.setItem("appLanguage", language).then((res) => {
+              console.log("appLangugae set", res);
+              const terms = data.result.terms;
+              if (terms.length > 0) {
+                var modifierJson = {};
+                terms.map((term) => {
+                  modifierJson[term.term] = term.translation.content;
+                });
 
-      if (response.data.response.status === "success") {
-        await AsyncStorage.setItem("appLanguage", language);
-        const terms = response.data.result.terms;
-        if (terms.length > 0) {
-          var modifierJson = {};
-          terms.map((term) => {
-            modifierJson[term.term] = term.translation.content;
-          });
+                I18nManager.allowRTL(language === "ar");
+                I18nManager.forceRTL(language === "ar");
+                // console.log("language getLanguageListPOEdit", language);
+                // console.log("modiferJson", modifierJson);
+                i18n.translations = {
+                  [language]: modifierJson,
+                };
 
-          I18nManager.allowRTL(language === "ar");
-          I18nManager.forceRTL(language === "ar");
-          // console.log("language getLanguageListPOEdit", language);
-          // console.log("modiferJson", modifierJson);
-          i18n.translations = {
-            [language]: modifierJson,
-          };
+                return dispatch({
+                  type: actionTypes.SET_LANGUAGE_LIST_POEDIT,
+                  payload: {
+                    terms: modifierJson,
+                    language,
+                  },
+                });
+              }
+            });
+          }
+        });
 
-          return dispatch({
-            type: actionTypes.SET_LANGUAGE_LIST_POEDIT,
-            payload: {
-              terms: modifierJson,
-              language,
-            },
-          });
-        }
-      } else {
-        const response = await createBaseUrl().get(`translation/${language}`);
-        const data = response.data;
-        // if this also fails then local copy
-        await AsyncStorage.setItem("appLanguage", language);
-        I18nManager.allowRTL(language === "ar");
-        I18nManager.forceRTL(language === "ar");
-        if (data) {
-          i18n.translations = {
-            [language]: data,
-          };
+      //   if (response.data.response.status === "success") {
+      //     await AsyncStorage.setItem("appLanguage", language);
+      //     const terms = response.data.result.terms;
+      //     if (terms.length > 0) {
+      //       var modifierJson = {};
+      //       terms.map((term) => {
+      //         modifierJson[term.term] = term.translation.content;
+      //       });
 
-          return dispatch({
-            type: actionTypes.SET_LANGUAGE_LIST_POEDIT,
-            payload: {
-              terms: data,
-              language,
-            },
-          });
-        } else {
-          i18n.translations = {
-            [language]: language === "ar" ? arabicStrings : englishStrings,
-          };
+      //       I18nManager.allowRTL(language === "ar");
+      //       I18nManager.forceRTL(language === "ar");
+      //       // console.log("language getLanguageListPOEdit", language);
+      //       // console.log("modiferJson", modifierJson);
+      //       i18n.translations = {
+      //         [language]: modifierJson,
+      //       };
 
-          return dispatch({
-            type: actionTypes.SET_LANGUAGE_LIST_POEDIT,
-            payload: {
-              terms: language === "ar" ? arabicStrings : englishStrings,
-              language,
-            },
-          });
-        }
-      }
+      //       return dispatch({
+      //         type: actionTypes.SET_LANGUAGE_LIST_POEDIT,
+      //         payload: {
+      //           terms: modifierJson,
+      //           language,
+      //         },
+      //       });
+      //     }
+      //   } else {
+      //     const response = await createBaseUrl().get(`translation/${language}`);
+      //     const data = response.data;
+      //     // if this also fails then local copy
+      //     await AsyncStorage.setItem("appLanguage", language);
+      //     I18nManager.allowRTL(language === "ar");
+      //     I18nManager.forceRTL(language === "ar");
+      //     if (data) {
+      //       i18n.translations = {
+      //         [language]: data,
+      //       };
+
+      //       return dispatch({
+      //         type: actionTypes.SET_LANGUAGE_LIST_POEDIT,
+      //         payload: {
+      //           terms: data,
+      //           language,
+      //         },
+      //       });
+      //     } else {
+      //       i18n.translations = {
+      //         [language]: language === "ar" ? arabicStrings : englishStrings,
+      //       };
+
+      //       return dispatch({
+      //         type: actionTypes.SET_LANGUAGE_LIST_POEDIT,
+      //         payload: {
+      //           terms: language === "ar" ? arabicStrings : englishStrings,
+      //           language,
+      //         },
+      //       });
+      //     }
+      //   }
     } catch (error) {
       // console.log("translation error", error.response || error.message);
 
