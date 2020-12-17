7950c32971d97074719877b6001750b9a3465f2c
saadiyakazi <saadiya@optimizeapp.com>
2020-12-17 12:46:50 +0300

    Changes to the ccampaign end date in progreess bar crash issue

--- components/MiniComponents/CampaignCircleCharts/CampDetailsInfo.js
+++ components/MiniComponents/CampaignCircleCharts/CampDetailsInfo.js
@@ -49,7 +49,7 @@ export default (props) => {
             progress={
               statusOfCampaign === "starts"
                 ? 0
-                : (TimeDifferance(campaign.start_time, currentDate) + 1) /
+                : TimeDifferance(campaign.start_time, currentDate) /
                   (TimeDifferance(campaign.start_time, campaign.end_time) === 0
                     ? 1
                     : TimeDifferance(campaign.start_time, campaign.end_time) +
