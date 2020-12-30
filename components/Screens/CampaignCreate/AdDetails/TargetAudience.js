b2edb5da61da8635dfbae93f8220722720757741
saadiyakazi <saadiya@optimizeapp.com>
2020-12-30 11:49:32 +0300

    Fix crash on TargetAUdiencce snapcat for device os type

--- components/Screens/CampaignCreate/AdDetails/TargetAudience.js
+++ components/Screens/CampaignCreate/AdDetails/TargetAudience.js
@@ -93,6 +93,14 @@ export class TargetAudience extends Component {
     } = this.props;
     const { translate } = this.props.screenProps;
     const { expandLocation, expandDemographics, expandDevices } = this.state;
+
+    let OsType = targeting.devices[0]
+      ? OSType.find((r) => r.value === targeting.devices[0].os_type)
+      : null;
+
+    if (OsType) {
+      OsType = translate(OsType.label);
+    }
     return (
       <View
         style={{
@@ -480,15 +488,7 @@ export class TargetAudience extends Component {
                   <Text style={styles.menutext}>
                     {translate("Operating System")}
                   </Text>
-                  <Text style={styles.menudetails}>
-                    {targeting.devices[0] &&
-                      translate(
-                        OSType.find((r) => {
-                          if (r.value === targeting.devices[0].os_type)
-                            return r;
-                        }).label
-                      )}
-                  </Text>
+                  <Text style={styles.menudetails}>{OsType}</Text>
                 </View>
 
                 {startEditing &&
