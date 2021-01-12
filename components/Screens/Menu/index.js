83a7e671f5db83da026374b1c82dc93f944932f8
saadiyakazi <saadiya@optimizeapp.com>
2021-01-12 15:22:24 +0300

    ota 345 with rejeccted ccampaign fix

--- components/Screens/Menu/index.js
+++ components/Screens/Menu/index.js
@@ -415,7 +415,7 @@ class Menu extends Component {
               </TouchableOpacity>
               <Text style={styles.version}>
                 {translate("Version:")}
-                {Constants.nativeAppVersion}/344/
+                {Constants.nativeAppVersion}/345/
                 {Constants.nativeBuildVersion}
               </Text>
             </ScrollView>
