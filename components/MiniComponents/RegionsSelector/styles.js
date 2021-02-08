499a34e6a9ea0dc74e3009aeaf73007f003d4909
saadiyakazi <saadiya@optimizeapp.com>
2021-02-08 18:03:56 +0300

    Select region modal, and google ad preview made responsive

--- components/MiniComponents/RegionsSelector/styles.js
+++ components/MiniComponents/RegionsSelector/styles.js
@@ -1,94 +1,95 @@
 import { StyleSheet, PixelRatio, StatusBar } from "react-native";
 import {
   widthPercentageToDP as wp,
-  heightPercentageToDP as hp
+  heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
+import { RFValue } from "react-native-responsive-fontsize";
 import { globalColors } from "../../../GlobalStyles";
 
 const styles = StyleSheet.create({
   selectTextContainer: {
-    paddingVertical: 20,
+    paddingVertical: RFValue(10, 414),
     flexDirection: "row",
     alignContent: "center",
     justifyContent: "space-between",
-    display: "flex"
+    display: "flex",
   },
   itemCircles: {
-    fontSize: 23,
-    color: globalColors.white
+    fontSize: RFValue(11.5, 414),
+    color: globalColors.white,
   },
   container: {
     flex: 1,
     alignItems: "center",
     flexDirection: "column",
-    justifyContent: "space-between"
+    justifyContent: "space-between",
   },
   dataContainer: {
     alignItems: "center",
     width: "100%",
     flex: 1,
-    justifyContent: "space-between"
+    justifyContent: "space-between",
   },
   slidercontainer: {
-    paddingHorizontal: 40,
-    width: "100%"
+    paddingHorizontal: RFValue(20, 414),
+    width: "100%",
   },
   searchInputText: {
     fontFamily: "montserrat-regular",
     color: "#fff",
-    fontSize: 14 / PixelRatio.getFontScale(),
+    fontSize: RFValue(7 / PixelRatio.getFontScale(), 414),
     borderBottomColor: "#0000",
     alignSelf: "center",
     textAlign: "center",
-    height: 37
+    height: RFValue(16.5, 414),
   },
   title: {
     textAlign: "center",
     color: "#fff",
     fontFamily: "montserrat-bold",
-    fontSize: 16,
-    paddingTop: 20,
-    alignSelf: "center"
+    fontSize: RFValue(8, 414),
+    paddingTop: RFValue(10, 414),
+    alignSelf: "center",
   },
   scrollContainer: {
-    marginTop: 15,
+    marginTop: RFValue(7.5, 414),
     width: "100%",
-    maxHeight: hp("45%")
+    maxHeight: hp("45%"),
   },
   regionText: {
     fontFamily: "montserrat-bold",
-    fontSize: 14,
+    fontSize: RFValue(7, 414),
     textAlign: "left",
     justifyContent: "center",
     alignSelf: "center",
-    paddingLeft: 10,
-    width: 180,
-    color: "#fff"
+    paddingLeft: RFValue(5, 414),
+    width: RFValue(90, 414),
+    color: "#fff",
   },
   regionTextView: {
     flexDirection: "row",
-    flex: 1
+    flex: 1,
   },
   reachText: {
     fontFamily: "montserrat-bold-english",
     color: "#fff",
-    fontSize: 14,
+    fontSize: RFValue(7, 414),
     justifyContent: "center",
-    alignSelf: "center"
+    alignSelf: "center",
   },
   searchbarContainer: {
-    marginBottom: 10,
-    marginTop: 20,
+    marginBottom: RFValue(5, 414),
+    marginTop: RFValue(10, 414),
     alignSelf: "center",
-    width: 300,
+    width: RFValue(150, 414),
     borderColor: "#0000",
     backgroundColor: "rgba(0,0,0,0.15)",
-    borderRadius: 30,
-    paddingHorizontal: 15
+    borderRadius: RFValue(15, 414),
+    paddingHorizontal: RFValue(7.5, 414),
   },
   regionsHeader: {
     flexDirection: "row",
-    justifyContent: "space-between"
-  }
+    justifyContent: "space-between",
+  },
 });
 export default styles;
