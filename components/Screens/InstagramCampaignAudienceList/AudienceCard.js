9afc8f98375df4ba84164461b030d56513971408
saadiyakazi <saadiya@optimizeapp.com>
2021-02-22 14:05:54 +0300

    Add Component for instagram audience list

---
+++ components/Screens/InstagramCampaignAudienceList/AudienceCard.js
@@ -0,0 +1,81 @@
+import React from "react";
+
+import { View, TouchableOpacity, Text } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+// STYLES
+import styles from "./styles";
+import { globalColors } from "../../../GlobalStyles";
+
+// DATA
+import countries from "../../Data/countries.billingAddress";
+// ICONS
+import PenIcon from "../../../assets/SVGs/Pen";
+import TrashIcon from "../../../assets/SVGs/Bin.svg";
+
+export default AudienceCard = (props) => {
+  const { item } = props;
+  const { translate } = props.screenProps;
+  let countryNames = item.targeting.geos.map((country) => country.country_code);
+  let updatedcountryNames = countries.filter((ctry) => {
+    return countryNames.indexOf(ctry.value) !== -1;
+  });
+  updatedcountryNames = updatedcountryNames.map((name) =>
+    translate(name.label)
+  );
+
+  return (
+    <View style={[styles.cardViewOut]}>
+      <TouchableOpacity
+        key={item.name}
+        style={[
+          styles.cardView,
+          props.selected_audience_id === item.id && styles.activeCardView,
+        ]}
+        onPress={() => {
+          props.setAudienceId(item.id);
+          props.setSelectedAudience(item.targeting, item.coordinates);
+
+          //   props.navigation.navigate("AdDetails", {
+          //     audienceSelected: true,
+          //     campaignTargeting: item.targeting,
+          //     coordinates: item.coordinates,
+          //     source: "audience_list",
+          //     source_action: "a_select_audience",
+          //   });
+        }}
+      >
+        <Text style={styles.audienceName}>{item.name}</Text>
+        <Text style={styles.countryNames}>
+          {updatedcountryNames.join(", ")}
+        </Text>
+      </TouchableOpacity>
+      <View style={styles.flexAddEdit}>
+        <TouchableOpacity
+          style={styles.editAudienceIcon}
+          onPress={() => {
+            props.getAudienceDetail(item.id);
+            props.navigation.navigate("SnapchatAudienceTagetting", {
+              editAudience: true,
+            });
+          }}
+        >
+          <PenIcon
+            width={RFValue(6.5, 414)}
+            height={RFValue(6.5, 414)}
+            fill={globalColors.purple}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.deleteAudienceIcon}
+          onPress={() => props.showAlert(item)}
+        >
+          <TrashIcon
+            width={RFValue(6.5, 414)}
+            height={RFValue(6.5, 414)}
+            fill={globalColors.purple}
+          />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
