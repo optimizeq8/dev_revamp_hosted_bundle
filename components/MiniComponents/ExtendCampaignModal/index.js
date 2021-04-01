ebd1f7a2b82be255b72d5a22845147437ca71af8
saadiyakazi <saadiya@optimizeapp.com>
2021-04-01 12:42:38 +0300

    Change source name for repeat ampiagn modal

--- components/MiniComponents/ExtendCampaignModal/index.js
+++ components/MiniComponents/ExtendCampaignModal/index.js
@@ -63,6 +63,12 @@ class ExtendCampaignModal extends Component {
   };
 
   handleSwitch = (value) => {
+    analytics.track("a_toggle_date_modal", {
+      source: "dashboard",
+      source_action: "a_toggle_date_modal",
+      visible: value,
+      campaign_channel: campaign.channel,
+    });
     this.setState({ switchComponent: value });
   };
   handleDateSubmition = () => {
