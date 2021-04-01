6e5cd3256c19c9662d428412fc347bb440d668ca
SamyOptimize <samy@optimizeapp.com>
2021-04-01 22:52:39 +0300

    Hide auto placemnt campaign
    OTA for live 375

--- components/Screens/Menu/index.js
+++ components/Screens/Menu/index.js
@@ -471,7 +471,7 @@ class Menu extends Component {
               </TouchableOpacity>
               <Text selectable style={styles.version}>
                 {translate("Version:")}
-                {Constants.nativeAppVersion}/374/
+                {Constants.nativeAppVersion}/375/
                 {Constants.nativeBuildVersion}
               </Text>
             </ScrollView>
