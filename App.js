e7f8418c44b238764d9093ae4df6a904f6b996f4
saadiyakazi <saadiya@optimizeapp.com>
2021-04-19 14:38:27 +0300

    log test

--- App.js
+++ App.js
@@ -530,6 +530,7 @@ class App extends React.Component {
   );
 
   render() {
+    console.log("test");
     let opacityNeg = {
       opacity: this.state.loadingProgress.interpolate({
         inputRange: [0, 25, 50],
