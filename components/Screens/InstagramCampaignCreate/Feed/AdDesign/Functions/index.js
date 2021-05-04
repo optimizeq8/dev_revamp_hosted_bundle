b6c1fb69cab537b7709d0d002639e3cd02937741
saadiyakazi <saadiya@optimizeapp.com>
2021-05-04 13:21:46 +0300

    Make cchanges for the existing_media format

--- components/Screens/InstagramCampaignCreate/Feed/AdDesign/Functions/index.js
+++ components/Screens/InstagramCampaignCreate/Feed/AdDesign/Functions/index.js
@@ -158,14 +158,16 @@ export const formatMedia = (
   body.append("campaign_id", campaignInfo.campaign_id);
   body.append("campaign_name", data.name);
   body.append("campaign_type", "InstagramFeedAd");
+
+  body.append("media_option", campaignInfo.media_option); //Oneof [single, carousel, collection]
   body.append("existing_media", campaignInfo.existing_media);
   if (campaignInfo.existing_media) {
     let res = media.split("/");
     res = res[res.length - 1];
     body.append("media", res);
     body.append("media_type", media_type);
+    body.append("media_option", campaignInfo.media_option); //Oneof [single, carousel, collection]
   }
-  body.append("media_option", campaignInfo.media_option); //Oneof [single, carousel, collection]
   body.append("message", campaignInfo.message);
   body.append(
     "destination",
