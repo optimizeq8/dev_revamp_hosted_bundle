58c264072d7d1caddcc594bf9daed637ae6b7c42
saadiyakazi <saadiya@optimizeapp.com>
2021-05-27 10:25:57 +0300

    Cache node module clean

--- android/app/src/main/java/com/optimizeapp/optimizeapp/MainApplication.java
+++ android/app/src/main/java/com/optimizeapp/optimizeapp/MainApplication.java
@@ -7,6 +7,7 @@ import android.util.Log;
 
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.horcrux.svg.SvgPackage;
 import com.robinpowered.react.Intercom.IntercomPackage;
 import com.airbnb.android.react.lottie.LottiePackage;
 // import com.segment.analytics.reactnative.integration.adjust.RNAnalyticsIntegration_AdjustPackage;
