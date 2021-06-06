2d390ea7fed9d5910598a7726c37879871a0b726
saadiyakazi <saadiya@optimizeapp.com>
2021-06-06 12:54:25 +0300

    Styling fixes on AD cover

--- components/MiniComponents/CampaignOptionsMenu/styles.js
+++ components/MiniComponents/CampaignOptionsMenu/styles.js
@@ -1,4 +1,5 @@
 import { StyleSheet } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
 import { globalColors } from "../../../GlobalStyles";
 
 export default StyleSheet.create({
@@ -7,18 +8,19 @@ export default StyleSheet.create({
     width: "100%",
     position: "absolute",
     bottom: 0,
-    borderTopLeftRadius: 20,
-    borderTopRightRadius: 20,
-    maxHeight: 250,
+    borderTopLeftRadius: RFValue(10, 414),
+    borderTopRightRadius: RFValue(10, 414),
+    maxHeight: RFValue(125, 414),
     height: "25%",
     borderColor: globalColors.rum,
     borderWidth: 0.5,
-    padding: 10,
+    padding: RFValue(5, 414),
   },
   menuHeaderText: {
     color: globalColors.rum,
     fontFamily: "montserrat-bold",
-    fontSize: 16,
+    fontSize: RFValue(8, 414),
     textTransform: "uppercase",
+    textAlign: "left",
   },
 });
