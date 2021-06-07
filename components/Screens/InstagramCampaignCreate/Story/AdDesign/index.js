64e411747b473262ec0d933d3bf00e98a7be57fd
saadiyakazi <saadiya@optimizeapp.com>
2021-06-07 15:51:05 +0300

    Fix crash for instagram story ad on missing data

--- components/Screens/InstagramCampaignCreate/Story/AdDesign/index.js
+++ components/Screens/InstagramCampaignCreate/Story/AdDesign/index.js
@@ -911,7 +911,7 @@ class AdDesign extends Component {
             this.props.instagramExistingMediaListLoading
           }
           setExistingMediaUrl={this.setExistingMediaUrl}
-          existing_media_url={this.props.data.media}
+          existing_media_url={this.props.data ? this.props.data.media : ""}
         />
       </View>
     );
