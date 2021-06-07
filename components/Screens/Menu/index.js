d07abd7ae5e1aee8108a204d9f7ddfb9fa15ca70
saadiyakazi <saadiya@optimizeapp.com>
2021-06-07 10:35:12 +0300

    ota 400 test flight

--- components/Screens/Menu/index.js
+++ components/Screens/Menu/index.js
@@ -472,7 +472,7 @@ class Menu extends Component {
               </TouchableOpacity>
               <Text selectable style={styles.version}>
                 {translate("Version:")}
-                {Constants.nativeAppVersion}/399/
+                {Constants.nativeAppVersion}/400/
                 {Constants.nativeBuildVersion}
               </Text>
             </ScrollView>
