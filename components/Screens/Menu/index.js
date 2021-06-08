a601b946afc67a89ab214cc23bd443c3aa395064
SamyOptimize <samy@optimizeapp.com>
2021-06-08 18:20:01 +0300

    Add ota bundle files

--- components/Screens/Menu/index.js
+++ components/Screens/Menu/index.js
@@ -472,7 +472,7 @@ class Menu extends Component {
               </TouchableOpacity>
               <Text selectable style={styles.version}>
                 {translate("Version:")}
-                {Constants.nativeAppVersion}/404/
+                {Constants.nativeAppVersion}/405/
                 {Constants.nativeBuildVersion}
               </Text>
             </ScrollView>
