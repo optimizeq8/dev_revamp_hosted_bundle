f4e56454218ca91f15d8ffe734dda3cab113e4dd
SamyOptimize <samy@optimizeapp.com>
2021-06-09 14:36:14 +0300

    Add OTA bundles for dev app 407

--- components/Screens/Menu/index.js
+++ components/Screens/Menu/index.js
@@ -472,7 +472,7 @@ class Menu extends Component {
               </TouchableOpacity>
               <Text selectable style={styles.version}>
                 {translate("Version:")}
-                {Constants.nativeAppVersion}/406/
+                {Constants.nativeAppVersion}/407/
                 {Constants.nativeBuildVersion}
               </Text>
             </ScrollView>
