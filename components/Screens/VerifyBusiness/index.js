ab9758d90e264fd574e9ba28d61b5d5aa308560a
saadiyakazi <saadiya@optimizeapp.com>
2021-06-30 14:57:27 +0300

    Add screen for verify business

---
+++ components/Screens/VerifyBusiness/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import { View } from "react-native";
+class VerifyBusiness extends React.Component {
+  render() {
+    return <View></View>;
+  }
+}
+
+export default VerifyBusiness;
